371da0e53bfd5e8674b016089857dece15264f2c
Honza Javorek <mail@honzajavorek.cz>
2019-08-06 20:00:05 +0200

    for some reason 'set -e' doesn't propagate exit status 1

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-set -e
-rm -r build && git checkout build && echo "Build directory reset done"
-python -m juniorguru.index.build && echo "Page index done"
-python -m juniorguru.learn.build && echo "Page learn done"
-python -m juniorguru.practice.build && echo "Page practice done"
-python -m juniorguru.jobs.build && echo "Page jobs done"
-python -m juniorguru.privacy.build && echo "Page privacy done"
+rm -r build && git checkout build && echo "Build directory reset done" || exit 1
+python -m juniorguru.index.build && echo "Page index done" || exit 1
+python -m juniorguru.learn.build && echo "Page learn done" || exit 1
+python -m juniorguru.practice.build && echo "Page practice done" || exit 1
+python -m juniorguru.jobs.build && echo "Page jobs done" || exit 1
+python -m juniorguru.privacy.build && echo "Page privacy done" || exit 1
