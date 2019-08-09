ae77758a5e4f26a9603b2cdf02a3bbbe8a589f51
Honza Javorek <mail@honzajavorek.cz>
2019-08-09 11:41:20 +0200

    rename build.py to __main__.py

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,5 +5,5 @@ for dir in juniorguru/pages/*/
 do
     dir=${dir%*/}  # remove the trailing '/'
     page=${dir##*/}  # everything after the final '/'
-    python -m "juniorguru.pages.$page.build" && echo "Page $page done" || exit 1
+    python -m "juniorguru.pages.$page" && echo "Page $page done" || exit 1
 done
