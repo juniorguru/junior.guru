2b73f5cc493e0e2e146d25d2d07f1fb88de6569e
Honza Javorek <mail@honzajavorek.cz>
2019-11-07 16:32:31 +0100

    use blcl
    inspired by https://twitter.com/alesroubicek/status/1191063063434317824
    (i.e. what https://github.com/topmonks/topmonks-webs use)

---
+++ scripts/check_links.py
@@ -0,0 +1,22 @@
+import sys
+from subprocess import run
+from pathlib import Path
+
+
+USER_AGENT = (
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:70.0) '
+    'Gecko/20100101 Firefox/70.0'
+)
+BUILD_DIR = Path(__file__).parent.parent / 'build'
+
+
+sys.exit(run([
+    'npx',
+    'blcl',
+    '--verbose',
+    '--follow',
+    '--recursive',
+    '--get',
+    f'--user-agent={USER_AGENT}',
+    BUILD_DIR,
+]).returncode)
