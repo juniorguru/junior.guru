060c80ccee2f73c0ec2da2f9417fd93b759fa524
Honza Javorek <mail@honzajavorek.cz>
2019-11-07 14:51:41 +0100

    add more arguments to pylinkvalidate.py

--- scripts/validate_links.py
+++ scripts/validate_links.py
@@ -9,7 +9,14 @@ try:
     while True:
         if 'http://localhost:3000' in server.stderr.readline().decode('utf8'):
             break
-    validate = run(['pylinkvalidate.py', 'http://localhost:3000'])
+    validate = run([
+        'pylinkvalidate.py',
+        '--test-outside',
+        '--progress',
+        '--verbose=2',
+        '--header=User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:70.0) Gecko/20100101 Firefox/70.0',
+        'http://localhost:3000'
+    ])
     returncode = validate.returncode
 
 finally:
