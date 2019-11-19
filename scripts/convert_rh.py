e4259a8db90eee6f5c0833bb2936f50e9a266a98
Honza Javorek <mail@honzajavorek.cz>
2019-11-19 13:37:03 +0100

    call pandoc in a subprocess and do additional edits

--- scripts/convert_rh.py
+++ scripts/convert_rh.py
@@ -1,9 +1,13 @@
-# To convert text from Red Hat job ads to Markdown. Usage:
-#
-# $ brew install pandoc
-# $ pipenv run python scripts/convert_rh.py 'https://...' | pandoc -f html -t markdown_strict-raw_html | pbcopy
+"""
+To convert text from Red Hat job ads to Markdown. Usage::
 
+    $ brew install pandoc
+    $ pipenv run python scripts/convert_rh.py 'https://...' | pbcopy
+"""
+
+import re
 import sys
+from subprocess import run
 
 import requests
 from lxml import html
@@ -26,4 +30,20 @@ job_content = html_tree.cssselect('.iCIMS_JobContent')[0]
 for rubbish in job_content.cssselect('.iCIMS_JobContent > :not(.iCIMS_InfoMsg)'):
     rubbish.getparent().remove(rubbish)
 
-print(html.tostring(job_content).decode('utf-8'))
+pandoc = run(['pandoc', '-f', 'html', '-t', 'markdown_strict-raw_html'],
+              input=html.tostring(job_content),
+              check=True,
+              capture_output=True)
+md = pandoc.stdout.decode('utf-8').strip()
+
+md = re.sub(r'\n[\xa0 ]+\n', '\n\n', md)
+md = re.sub(r'\n{2,}', '\n\n', md)
+
+assert not re.search(r'^={3,}$', md, flags=re.M)
+assert not re.search(r'^#{1,} ', md, flags=re.M)
+
+def repl(match):
+    return '=' * len(match.group(0))
+md = re.sub(r'^\-{3,}$', repl, md, flags=re.M)
+
+print(md)
