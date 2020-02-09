a67bff4cd59c8bcc756c623a08332d72ca421432
Honza Javorek <mail@honzajavorek.cz>
2020-02-09 20:38:06 +0100

    make now CLI non-interactive

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,4 +9,4 @@ fi
 if [[ -n "$NOW_TARGET" ]]; then
     now_target_option="--target=$NOW_TARGET"
 fi
-now "$now_token_option" "$now_target_option"
+now --confirm --no-clipboard "$now_token_option" "$now_target_option"
