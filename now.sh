cd2ffd6d628c86a0206234b64e798d615eb7cb2e
Honza Javorek <mail@honzajavorek.cz>
2020-02-10 21:38:40 +0100

    fix a typo

--- now.sh
+++ now.sh
@@ -8,7 +8,7 @@ pip install pipenv
 curl https://pyenv.run | bash
 source ~/.bashrc
 pyenv install 3.7.5
-pipenv install --dev --python="$(pyenv prefix 3.6.6)/bin/python"
+pipenv install --dev --python="$(pyenv prefix 3.7.5)/bin/python"
 
 # Build
 pipenv run fetch  # needs $GOOGLE_SERVICE_ACCOUNT
