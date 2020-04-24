1ca2e144c55756afe7940d258f1df4aaad7fd382
Honza Javorek <mail@honzajavorek.cz>
2020-04-24 14:58:40 +0200

    add scrapers check

---
+++ scripts/check_scrapers.py
@@ -0,0 +1,20 @@
+import sys
+import json
+from pathlib import Path
+
+
+path = Path('juniorguru/data/jobs/errors.json')
+try:
+    contents = path.read_text()
+    errors = json.loads(contents)
+except IOError:
+    print(f"File '{path}' doesn't exist!", file=sys.stderr)
+    sys.exit(1)
+except json.JSONDecodeError:
+    print(f"File '{path}' doesn't parse as JSON!", file=sys.stderr)
+    sys.exit(1)
+
+if errors:
+    print(f"File '{path}' contains errors!", file=sys.stderr)
+    sys.exit(1)
+print('OK', file=sys.stderr)
