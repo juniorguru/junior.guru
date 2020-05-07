a49a4e2c4ca6c8a64c95c4fc49fb8746a0e6f71b
Honza Javorek <mail@honzajavorek.cz>
2020-05-07 15:44:02 +0200

    track clicks on apply buttons

--- juniorguru/web/static/src/js/jobs.js
+++ juniorguru/web/static/src/js/jobs.js
@@ -9,3 +9,27 @@ function onHashChange() {
 
 window.addEventListener('hashchange', onHashChange);
 document.addEventListener('DOMContentLoaded', onHashChange);
+
+function onApply(url) {
+  try {
+    gtag('event', 'click', {
+      'event_category': 'apply',
+      'event_label': url,
+      'transport_type': 'beacon',
+    });
+  } catch (error) {
+    // do nothing, sending the event to GA is just 'nice to have'
+  }
+}
+
+function onLoad() {
+  if (!gtag) return;
+  if (document.body.className != 'page-job') return;
+  const applyButton = document.getElementById('apply');
+  if (!applyButton) return;
+  applyButton.addEventListener('mousedown', function (event) {
+    onApply(window.location.href);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', onLoad);
