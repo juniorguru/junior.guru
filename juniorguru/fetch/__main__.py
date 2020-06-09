65cc35bd2f73dfc43ae59389814b04aadb3cab04
Honza Javorek <mail@honzajavorek.cz>
2020-06-09 16:36:44 +0200

    add metrics to the global fetch command

--- juniorguru/fetch/__main__.py
+++ juniorguru/fetch/__main__.py
@@ -1,10 +1,12 @@
 from juniorguru.fetch.fetch_jobs import main as fetch_jobs
+from juniorguru.fetch.fetch_metrics import main as fetch_metrics
 from juniorguru.fetch.fetch_stories import main as fetch_stories
 
 
 def main():
     fetch_jobs()
     fetch_stories()
+    fetch_metrics()
 
 
 if __name__ == '__main__':
