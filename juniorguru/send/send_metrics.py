18a9d879827077a17c28be2d997ca4fdc1802b69
Honza Javorek <mail@honzajavorek.cz>
2020-06-12 18:01:17 +0200

    use tables

--- juniorguru/send/send_metrics.py
+++ juniorguru/send/send_metrics.py
@@ -32,7 +32,7 @@ def main():
     template_path = Path(__file__).parent / 'templates' / 'metrics.html'
     template = Template(template_path.read_text())
 
-    for job in jobs[:2]:  # DEBUG
+    for job in jobs[:1]:  # DEBUG
         send(create_message(today, job, template))
 
 
