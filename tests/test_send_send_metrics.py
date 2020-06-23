18b6e0322088f4115c975e76737940e066b3cd46
Honza Javorek <mail@honzajavorek.cz>
2020-06-23 19:35:07 +0200

    split tests

--- tests/test_send_send_metrics.py
+++ tests/test_send_send_metrics.py
@@ -22,7 +22,10 @@ class JobMock():
 
 @pytest.fixture
 def job_mock():
-    return JobMock(**prepare_job_data('123'))
+    data = prepare_job_data('123')
+    return JobMock(effective_approved_at=data['approved_at'],
+                   metrics=dict(users=15, pageviews=25, applications=3),
+                   **data)
 
 
 @pytest.fixture
@@ -31,20 +34,31 @@ def template():
     return Template(template_path.read_text())
 
 
-def test_create_message(job_mock, template):
-    job_mock.company_name = 'Honza Ltd.'
-    job_mock.effective_approved_at = date(2020, 6, 1)
-    job_mock.expires_at = date(2020, 7, 1)
+def test_create_message_metrics(job_mock, template):
     job_mock.metrics = dict(users=15, pageviews=25, applications=3)
-
     message = create_message(job_mock, template, today=date(2020, 6, 23))
     html = message.get()['content'][0]['value']
-    print(html)
+
+    assert '<b>15</b>' in html
+    assert '<b>25</b>' in html
+
+
+def test_create_message_prefill_form(job_mock, template):
+    job_mock.company_name = 'Honza Ltd.'
+    message = create_message(job_mock, template, today=date(2020, 6, 23))
+    html = message.get()['content'][0]['value']
+
+    assert '&entry.681099058=Honza+Ltd.' in html
+
+
+def test_create_message_start_end(job_mock, template):
+    job_mock.approved_at = date(2020, 6, 1)
+    job_mock.effective_approved_at = date(2020, 6, 1)
+    job_mock.expires_at = date(2020, 7, 1)
+    message = create_message(job_mock, template, today=date(2020, 6, 23))
+    html = message.get()['content'][0]['value']
 
     assert '22&nbsp;dní' in html
     assert 'schválen 1.6.2020' in html
     assert '8&nbsp;dní' in html
     assert 'vyprší 1.7.2020' in html
-    assert '<b>15</b>' in html
-    assert '<b>25</b>' in html
-    assert '&entry.681099058=Honza+Ltd.' in html
