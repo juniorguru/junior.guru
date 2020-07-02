482778a0478a854c7872e182da4e14633fc97e95
Honza Javorek <mail@honzajavorek.cz>
2020-07-02 20:08:15 +0200

    make the header sticky & :target headings visible

--- juniorguru/web/static/src/js/more.js
+++ juniorguru/web/static/src/js/more.js
@@ -1,6 +1,13 @@
 function onLoad() {
   const sections = Array.from(document.getElementsByClassName('more'));
   sections.forEach(function (section) {
+    const div = document.createElement('div');
+    div.classList.add('more-wrapper');
+    Array.from(section.childNodes).forEach(function (child) {
+      div.appendChild(child);
+    });
+    section.appendChild(div);
+
     const p = document.createElement('p');
     p.classList.add('call-to-action');
     p.classList.add('more-action-area');
@@ -9,13 +16,13 @@ function onLoad() {
     button.textContent = 'Zobrazit víc';
     button.classList.add('legacy-button');
     button.addEventListener('click', function (event) {
-      section.removeChild(p);
-      section.classList.remove('more-collapsed');
+      div.removeChild(p);
+      div.classList.remove('more-collapsed');
     });
     p.appendChild(button);
 
-    section.classList.add('more-collapsed');
-    section.appendChild(p);
+    div.classList.add('more-collapsed');
+    div.appendChild(p);
   });
   handleHash();
 }
@@ -33,10 +40,10 @@ function handleHash() {
 
   let el = element;
   while (el) {
-    console.log(el);
+    // console.log(el);
     if (el.className.match('more-collapsed')) {
       const button = el.querySelector('.more-action-area .button');
-      console.log(button);
+      // console.log(button);
       button.dispatchEvent(new MouseEvent('click'));
       break;
     }
