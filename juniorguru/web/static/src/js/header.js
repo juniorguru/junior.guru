61be2a8ec81b63bf704ad3ccf4596c12df44104a
Honza Javorek <mail@honzajavorek.cz>
2020-08-04 15:42:15 +0200

    subnav must be on the left otherwise scrolling it on phones gets weird

--- juniorguru/web/static/src/js/header.js
+++ juniorguru/web/static/src/js/header.js
@@ -5,6 +5,7 @@ let headings = [];
 let tocHeading;
 
 document.addEventListener('DOMContentLoaded', function () {
+  // set variables
   tocHeading = document.getElementById('toc-heading');
   headings = [
     'intro__title',
@@ -15,6 +16,19 @@ document.addEventListener('DOMContentLoaded', function () {
   }).reduce(function (acc, arr) {
     return acc.concat(arr);
   }, []);
+
+  // if the subnav scrolls horizontally, set it to the middle or to make
+  // the active tab visible so it's clear on small screens that users can
+  // scroll it and no tabs get hidden
+  const subnav = Array.from(document.getElementsByClassName('header__subnav'))[0];
+  const activeTab = Array.from(document.getElementsByClassName('header__subnav-tab-control--active'))[0];
+
+  if (subnav && activeTab && subnav.scrollWidth > window.innerWidth) {
+    subnav.scrollLeft = Math.max(
+      (subnav.scrollWidth - window.innerWidth) / 2,
+      activeTab.getBoundingClientRect().left - 10
+    );
+  }
 });
 
 onScroll(function () {
