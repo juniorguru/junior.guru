5f3095df0510ded7496da76bbfc66c8cc921d715
Honza Javorek <mail@honzajavorek.cz>
2020-08-11 10:24:24 +0200

    add an arrow advertising the ToC toggle

--- juniorguru/web/static/src/js/toc.js
+++ juniorguru/web/static/src/js/toc.js
@@ -1,5 +1,5 @@
-function toggleToc(toc, tocToggle) {
-  if (tocToggle.checked) {
+function toggleToc(toc, checkbox) {
+  if (checkbox.checked) {
     toc.classList.add('toc--visible');
   } else {
     toc.classList.remove('toc--visible');
@@ -8,13 +8,21 @@ function toggleToc(toc, tocToggle) {
 
 document.addEventListener('DOMContentLoaded', function () {
   const toc = document.getElementById('toc');
-  const tocToggle = document.getElementById('toc-toggle');
+  const toggle = document.getElementsByClassName('toc-toggle')[0];
+  const checkbox = document.getElementById('toc-toggle-checkbox');
 
-  if (toc && tocToggle) {
-    tocToggle.addEventListener('change', function (event) {
-      toggleToc(toc, tocToggle);
+  if (toc && toggle && checkbox) {
+    checkbox.addEventListener('change', function (event) {
+      if (checkbox.checked) {
+        toggle.classList.remove('toc-toggle--unused');
+      }
+      toggleToc(toc, checkbox);
     });
-    toggleToc(toc, tocToggle);
+
+    if (!checkbox.checked) {
+      toggle.classList.add('toc-toggle--unused');
+    }
+    toggleToc(toc, checkbox);
 
     document.addEventListener('click', function (event) {
       let el = event.target;
@@ -25,8 +33,8 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         el = el.parentElement;
       }
-      tocToggle.checked = false;
-      tocToggle.dispatchEvent(new Event('change'));
+      checkbox.checked = false;
+      checkbox.dispatchEvent(new Event('change'));
     });
   }
 });
