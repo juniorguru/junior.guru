a20b6ca49bbf517c239391c0ebc7d061e5b2bd56
Honza Javorek <mail@honzajavorek.cz>
2020-08-21 12:56:40 +0200

    forgot to put last modified to global fetch command

--- juniorguru/fetch/__main__.py
+++ juniorguru/fetch/__main__.py
@@ -2,12 +2,14 @@ from juniorguru.fetch.fetch_jobs import main as fetch_jobs
 from juniorguru.fetch.fetch_metrics import main as fetch_metrics
 from juniorguru.fetch.fetch_stories import main as fetch_stories
 from juniorguru.fetch.fetch_supporters import main as fetch_supporters
+from juniorguru.fetch.fetch_last_modified import main as fetch_last_modified
 
 
 def main():
     # order-insensitive
     fetch_stories()
     fetch_supporters()
+    fetch_last_modified()
 
     # order-sensitive
     fetch_jobs()
