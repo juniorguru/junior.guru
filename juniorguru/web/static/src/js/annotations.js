43215e874a589abd70ed32b653adb318aa374473
Honza Javorek <mail@honzajavorek.cz>
2020-10-08 12:20:07 +0200

    annotate on scroll

--- juniorguru/web/static/src/js/annotations.js
+++ juniorguru/web/static/src/js/annotations.js
@@ -1,13 +1,45 @@
 import { annotate } from 'rough-notation';
 
 
+const observer = IntersectionObserver
+  ? new IntersectionObserver(handleIntersection, { threshold: [1] })
+  : null;
+let annotations;
+
+
 document.addEventListener('DOMContentLoaded', function () {
-  Array.from(document.querySelectorAll('*[data-annotate]')).forEach(function (element) {
-    const annotation = annotate(element, {
-      type: 'circle',
-      color: '#1755d1',
-      padding: 20,
+  annotations = Array.from(document.querySelectorAll('*[data-annotate]'))
+    .map(function (element, i) {
+      const id = i.toString();
+      const annotation = annotate(element, {
+        type: 'circle',
+        color: '#1755d1',
+        padding: 20,
+        animationDuration: 1600,
+      });
+      if (observer) {
+        element.dataset.annotateId = id;
+        observer.observe(element);
+      } else {
+        annotation.show();
+      }
+      return { id: id, annotation: annotation };
     });
-    annotation.show();
-  });
 });
+
+
+function handleIntersection(entries, observer) {
+  entries.filter(function (entry) {
+    return entry.isIntersecting;
+  }).forEach(function (entry) {
+    const element = entry.target;
+    const annotation = annotations.filter(function (item) {
+      return item.id === element.dataset.annotateId;
+    })[0].annotation;
+    if (annotation) {
+      annotation.show();
+    } else {
+      console.error("Couldn't find annotation");
+    }
+  });
+}
