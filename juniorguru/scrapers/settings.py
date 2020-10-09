e7a059042386f370422e5c50237d93bbaef1638a
Honza Javorek <mail@honzajavorek.cz>
2020-10-09 16:27:54 +0200

    try autothrottle

--- juniorguru/scrapers/settings.py
+++ juniorguru/scrapers/settings.py
@@ -103,7 +103,7 @@ IMAGES_STORE = 'juniorguru/data/images'
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
 #AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
