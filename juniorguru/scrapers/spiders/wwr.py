ce0536cb593ad7441e3a00425a9c22660517064d
Honza Javorek <mail@honzajavorek.cz>
2020-10-29 12:07:12 +0100

    rewrite WWR to use structured data & add tests

--- juniorguru/scrapers/spiders/wwr.py
+++ juniorguru/scrapers/spiders/wwr.py
@@ -1,41 +1,78 @@
+import html
+import time
+from datetime import datetime
+
+import arrow
+import extruct
+import feedparser
 from scrapy import Spider as BaseSpider
 from scrapy.loader import ItemLoader
 from itemloaders.processors import Identity, MapCompose, TakeFirst
 
-from juniorguru.scrapers.items import Job, absolute_url, parse_iso_date
+from juniorguru.scrapers.items import Job, absolute_url
 
 
 class Spider(BaseSpider):
     name = 'wwr'
     start_urls = [
-        'https://weworkremotely.com/categories/remote-devops-sysadmin-jobs',
-        'https://weworkremotely.com/categories/remote-programming-jobs',
+        'https://weworkremotely.com/categories/remote-devops-sysadmin-jobs.rss',
+        'https://weworkremotely.com/categories/remote-programming-jobs.rss',
     ]
 
     def parse(self, response):
-        for link in response.css('.feature a[href*="/remote-jobs/"]'):
-            yield response.follow(link, callback=self.parse_job)
+        for entry in feedparser.parse(response.text).entries:
+            feed_data = dict(title=entry.title,
+                             posted_at=parse_struct_time(entry.published_parsed),
+                             remote_region_raw=entry.region,
+                             company_logo_urls=[c['url'] for c in getattr(entry, 'media_content', [])],
+                             description_html=entry.summary,
+                             remote=True)
+            yield response.follow(entry.link,
+                                  callback=self.parse_job,
+                                  cb_kwargs=dict(feed_data=feed_data))
 
-    def parse_job(self, response):
+    def parse_job(self, response, feed_data):
         loader = Loader(item=Job(), response=response)
-        loader.add_css('title', 'h1::text')
         loader.add_value('link', response.url)
-        loader.add_css('company_name', '.company-card h2 a::text')
-        loader.add_css('company_link', '.company-card a::attr(href)')
-        loader.add_css('location_raw', '.company-card h2 ~ h3::text')
-        # loader.add_value('employment_types', ...) TODO
-        # loader.add_value('remote_region_raw', ...) TODO
-        loader.add_value('remote', True)
-        loader.add_css('posted_at', '.content time::attr(datetime)')
-        loader.add_css('description_html', '#job-listing-show-container')
-        loader.add_css('company_logo_urls', '.company-card .listing-logo img::attr(src)')
+
+        for key, value in feed_data.items():
+            loader.add_value(key, value)
+
+        data = extract_job_posting(response.text, response.url)
+        loader.add_value('title', data['title'])
+        loader.add_value('posted_at', data['datePosted'])
+        loader.add_value('description_html', html.unescape(data['description']))
+        loader.add_value('company_logo_urls', data['image'])
+        loader.add_value('employment_types', [data['employmentType']])
+        loader.add_value('company_name', data['hiringOrganization']['name'])
+        loader.add_value('company_link', data['hiringOrganization']['sameAs'])
+        loader.add_value('location_raw', data['hiringOrganization']['address'])
+
         yield loader.load_item()
+
+
+def parse_struct_time(struct_time):
+    if struct_time:
+        return datetime.fromtimestamp(time.mktime(struct_time)).date()
+
+
+def parse_date(value):
+    try:
+        return arrow.get(value).date()
+    except ValueError:
+        return arrow.get(value, 'YYYY-MM-DD HH:mm:ss ZZZ').date()
+
+
+def extract_job_posting(html_string, base_url):
+    data = extruct.extract(html_string, base_url, syntaxes=['json-ld'])
+    return next(data_item for data_item in data['json-ld']
+                if data_item['@type'] == 'JobPosting')
 
 
 class Loader(ItemLoader):
     default_input_processor = MapCompose(str.strip)
     default_output_processor = TakeFirst()
     company_link_in = MapCompose(absolute_url)
-    posted_at_in = MapCompose(parse_iso_date)
+    posted_at_in = MapCompose(parse_date)
     company_logo_urls_out = Identity()
     remote_in = MapCompose(bool)
