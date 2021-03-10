523902042e8af520ff54453dfed0364bdeed6d79
miiila <me@milavotradovec.cz>
2021-03-10 23:00:23 +0100

    Way to download all the archived artifacts

---
+++ scripts/downloader.py
@@ -0,0 +1,77 @@
+import asyncio
+import os
+
+from requests import get
+
+
+DOWNLOAD_DIR = './_downloaded'
+PROJECT_URL = 'https://circleci.com/api/v1.1/project/github/honzajavorek/junior.guru'
+
+
+def is_fetch_job(job):
+    return job.get('workflows', {}).get('job_name', '') == 'fetch'
+
+
+def is_nightly_job(job):
+    return job.get('workflows', {}).get('workflow_name', '') == 'nightly'
+
+
+def is_fetch_and_nightly(job):
+    return is_fetch_job(job) and is_nightly_job(job)
+
+
+def fetch_url(build_num):
+    try:
+        res = get(f'{PROJECT_URL}/{build_num}/artifacts')
+        artifacts = list(filter(lambda artifact: artifact.get('path', '') == 'backup.tar.gz', res.json())) 
+        # only one artifact should be there
+        artifact = artifacts[0] if len(artifacts) > 0 else {}
+
+        return artifact.get('url', None)
+    except Exception as e:
+        # something has failed, but no big deal
+        print (e);
+        pass
+
+
+def download_data(path, url):
+    with open(path, 'wb') as f:
+        for chunk in get(url, stream = True).iter_content(chunk_size = 1024*1024*3):
+            f.write(chunk)
+
+
+def fetch_artifacts(offset, loop):
+    os.makedirs(DOWNLOAD_DIR, exist_ok = True)
+    res = get(f'{PROJECT_URL}?limit=100&offset={offset}')
+    jobs = res.json()
+    fetch_and_nightly_jobs = filter(is_fetch_and_nightly, jobs)
+    build_nums = filter(lambda x: x is not None, map(lambda job: job.get('build_num', None), fetch_and_nightly_jobs))
+    appended = False
+    for build_num in build_nums:
+        url = fetch_url(build_num)
+        if url is None:
+            continue
+        print(f'Downloading {url}');
+        # Schedule downloading in the event loop
+        loop.run_in_executor(None, download_data, f'{DOWNLOAD_DIR}/{build_num}_backup.tar.gz', url)
+        appended = True
+    return appended
+
+
+def main():
+    offset = -1
+    # how many times no artifacts were returned
+    empty_times = 0
+    # if no artifacts for three consecutive calls, bail out
+    print('Starting artifacts finding and scheduling downloads')
+    loop = asyncio.get_event_loop()
+    while empty_times < 3:
+        offset += 1
+        appended = fetch_artifacts(offset * 100, loop)
+        # increase empty counter, otherwise reset it to 0 to cover a gap
+        empty_times += 1 if not appended else 0
+    print(f'Finished finding artifacts, last offset was {offset * 100}')
+
+
+if __name__ == "__main__":
+   main()
