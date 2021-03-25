f29c728d4a8c4a4d9f45742d16860547f8116080
Honza Javorek <mail@honzajavorek.cz>
2021-03-25 13:03:01 +0100

    make the PoC worky

--- juniorguru/mkdocs/hooks.py
+++ juniorguru/mkdocs/hooks.py
@@ -1,10 +1,13 @@
 import arrow
 
+from juniorguru.web import NAV_TABS
+from juniorguru.web.thumbnail import thumbnail
+
 
 def on_env(env, config, files):
     env.globals.update(dict(
         now=arrow.utcnow(),
+        nav_tabs=NAV_TABS,
+        thumbnail=thumbnail(),
+        handbook_release_at=arrow.get(2020, 9, 1),
     ))
-    # nav_tabs=NAV_TABS,
-    # handbook_release_at=arrow.get(2020, 9, 1),
-    # thumbnail=thumbnail()
