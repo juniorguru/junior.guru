cbedfa34be3e5ee0dacacc046b0ac795a84c6818
Honza Javorek <mail@honzajavorek.cz>
2021-04-08 16:52:21 +0200

    use macros for HTML inside Markdown documents

--- juniorguru/mkdocs/main.py
+++ juniorguru/mkdocs/main.py
@@ -5,12 +5,6 @@ from juniorguru.web.thumbnail import thumbnail
 
 
 def define_env(env):
-    # env.conf['extra'].update(dict(
-    #     now=arrow.utcnow(),
-    #     nav_tabs=NAV_TABS,
-    #     thumbnail=thumbnail(),
-    #     handbook_release_at=arrow.get(2020, 9, 1),
-    # ))
     pass
 
 
