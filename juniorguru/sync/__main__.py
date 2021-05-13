55326d119a442692b38bd1476fbe23e78f066e59
Honza Javorek <mail@honzajavorek.cz>
2021-05-13 14:04:26 +0200

    improve life

--- juniorguru/sync/__main__.py
+++ juniorguru/sync/__main__.py
@@ -1,3 +1,7 @@
+import sys
+import importlib
+
+from juniorguru.lib.log import get_log
 from juniorguru.sync.jobs import main as sync_jobs, manage_jobs_voting_channel
 from juniorguru.sync.logos import main as sync_logos
 from juniorguru.sync.metrics import main as sync_metrics
@@ -14,6 +18,9 @@ from juniorguru.sync.messages import main as sync_messages
 from juniorguru.sync.topics import main as sync_topics
 from juniorguru.sync.roles import main as sync_roles
 from juniorguru.lib.magic import do_magic
+
+
+log = get_log('sync')
 
 
 def main():
@@ -42,4 +49,15 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        module_name = f'juniorguru.sync.{sys.argv[1]}'
+    except IndexError:
+        # Standard `pipenv run sync`, i.e. `pipenv run python -m juniorguru.sync`
+        main()
+    else:
+        # For debugging purposes, one can run `pipenv run sync stories`,
+        # which is a shortcut for `pipenv run python -m juniorguru.sync.stories`
+        log.info(f"Running only: {module_name}")
+        module = importlib.import_module(module_name)
+        module.main()
+    log.info('Synchronization done!')
