d02fa24e1dd1c0b3bc580a36437894c58ae2a566
Honza Javorek <mail@honzajavorek.cz>
2021-06-10 16:14:01 +0200

    store original size of images to EXIF, then sort them before choosing

--- juniorguru/scrapers/pipelines/company_logo.py
+++ juniorguru/scrapers/pipelines/company_logo.py
@@ -1,6 +1,8 @@
 import hashlib
 from io import BytesIO
+from pathlib import Path
 
+import piexif
 from PIL import Image, ImageChops, ImageOps, UnidentifiedImageError
 
 from scrapy.pipelines.images import ImagesPipeline, ImageException
@@ -13,23 +15,41 @@ class Pipeline(ImagesPipeline):
     DEFAULT_IMAGES_RESULT_FIELD = 'company_logos'
 
     max_size_px = 1000
+    min_square_size_px = 100
     size_px = 100
 
     def __init__(self, store_uri, *args, **kwargs):
         self.images_dir = store_uri
-        self.orig_sizes = {}
         super().__init__(store_uri, *args, **kwargs)
 
     def item_completed(self, results, item, info):
         item = super().item_completed(results, item, info)
-        for company_logo in item.get(self.DEFAULT_IMAGES_RESULT_FIELD, []):
-            try:
-                width, height = self.orig_sizes.pop(company_logo['path'])  # TODO
-            except:
-                pass
-            item['company_logo_path'] = 'images/' + company_logo['path']
-            break
+
+        def sort_key(key_value):
+            orig_width, orig_height = key_value[1]
+            orig_area = orig_width * orig_height
+            if orig_width < self.min_square_size_px or orig_height < self.min_square_size_px:
+                return (0, -1 * orig_area)
+            similarity_to_square = abs(orig_width - orig_height)  # 0 means it is square
+            return (similarity_to_square, -1 * orig_area)
+
+        paths = [tuple_[0] for tuple_ in sorted([
+            (company_logo['path'], self.get_orig_size(company_logo['path']))
+            for company_logo
+            in item.get(self.DEFAULT_IMAGES_RESULT_FIELD, [])
+        ], key=sort_key)]
+
+        if paths:
+            item['company_logo_path'] = f"images/{paths[0]}"
         return item
+
+    def get_orig_size(self, path):
+        image = Image.open(Path(self.images_dir) / path)
+        exif = piexif.load(image.info['exif'])
+        try:
+            return exif['0th'][piexif.ImageIFD.XResolution][0], exif['0th'][piexif.ImageIFD.YResolution][0]
+        except (IndexError, KeyError):
+            return 0, 0
 
     def image_downloaded(self, response, request, info, item=None):
         path = self.file_path(request, response=response, info=info)
@@ -42,7 +62,6 @@ class Pipeline(ImagesPipeline):
         width, height = orig_image.size
         if width > self.max_size_px or height > self.max_size_px:
             raise ImageException(f'Image too large ({width}x{height} < {self.max_size_px}x{self.max_size_px})')
-        self.orig_sizes[path] = (width, height)
 
         image, buffer = self.convert_image(orig_image)
         buffer.seek(0)
@@ -54,7 +73,9 @@ class Pipeline(ImagesPipeline):
                                 headers={'Content-Type': 'image/png'})
         return checksum
 
-    def convert_image(self, image, size=None):
+    def convert_image(self, image):
+        orig_width, orig_height = image.size
+
         # transparent to white
         image = image.convert('RGBA')
         background = Image.new('RGBA', image.size, (255, 255, 255))
@@ -74,8 +95,17 @@ class Pipeline(ImagesPipeline):
         # resize
         image = image.resize((self.size_px, self.size_px))
 
+        # meta data
+        exif = piexif.dump({
+            '0th': {
+                piexif.ImageIFD.Make: 'junior.guru',
+                piexif.ImageIFD.XResolution: (orig_width, 1),
+                piexif.ImageIFD.YResolution: (orig_height, 1),
+            }
+        })
+
         buffer = BytesIO()
-        image.save(buffer, 'PNG')
+        image.save(buffer, 'PNG', exif=exif)
         return image, buffer
 
     def file_path(self, request, response=None, info=None, item=None):
