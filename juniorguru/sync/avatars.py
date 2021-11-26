3e7df3e450c1d32aa2002b34398b0d70b1ef32b0
Honza Javorek <mail@honzajavorek.cz>
2021-11-26 15:31:14 +0100

    improve output of the avatars script

--- juniorguru/sync/avatars.py
+++ juniorguru/sync/avatars.py
@@ -34,16 +34,17 @@ async def main(client):
 
 
 async def process_member(client, member):
-    user_logger = loggers.get(f'avatars.{member.id}')
-    user_logger.debug(f"Checking avatar of '{member.display_name}'")
+    member_logger = loggers.get(f'avatars.{member.id}')
+    member_logger.info(f"Checking avatar of member #{member.id}")
+    member_logger.debug(f"Member #{member.id} is named '{member.display_name}'")
     discord_member = await client.juniorguru_guild.fetch_member(member.id)
     if discord_member.avatar:
-        user_logger.debug(f"User '{member.display_name}' has avatar, downloading")
+        member_logger.debug(f"Member '{member.display_name}' has avatar, downloading")
         member.avatar_path = await download_avatar(discord_member.avatar)
     if member.avatar_path:
-        user_logger.debug(f"User '{member.display_name}' has avatar, downloaded as '{member.avatar_path}'")
+        member_logger.debug(f"Member '{member.display_name}' has avatar, downloaded as '{member.avatar_path}'")
     else:
-        user_logger.debug(f"User '{member.display_name}' has no avatar")
+        member_logger.debug(f"Member '{member.display_name}' has no avatar")
     member.save()
 
 
