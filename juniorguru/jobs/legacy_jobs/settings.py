eacbe464a6637eb0e7c92870bcdc2b08cc16ebb8
Honza Javorek <mail@honzajavorek.cz>
2022-02-01 17:04:41 +0100

    update settings

--- juniorguru/jobs/legacy_jobs/settings.py
+++ juniorguru/jobs/legacy_jobs/settings.py
@@ -1,70 +1,30 @@
 from datetime import date
 
-# Scrapy settings for jobs project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     https://docs.scrapy.org/en/latest/topics/settings.html
-#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
+today = date.today()
 BOT_NAME = 'jobs'
 
-SPIDER_MODULES = ['juniorguru.sync.jobs.spiders']
-NEWSPIDER_MODULE = 'juniorguru.sync.jobs.spiders'
+SPIDER_MODULES = ['juniorguru.sync.legacy_jobs.spiders']
+NEWSPIDER_MODULE = 'juniorguru.sync.legacy_jobs.spiders'
 
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'JuniorGuruBot (+https://junior.guru)'
-
-# Obey robots.txt rules
+USER_AGENT = f'JuniorGuruBot/{today.year}.{today.month}.{today.day} (+https://junior.guru)'
 ROBOTSTXT_OBEY = True
 
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
-
-# Configure a delay for requests for the same website (default: 0)
-# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
-# The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
-
-# Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
-
-# Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
-
-# Override the default request headers:
 DEFAULT_REQUEST_HEADERS = {
   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
   'Accept-Language': 'cs;q=0.8,en;q=0.6',
 }
 
-# Enable or disable spider middlewares
-# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'juniorguru.sync.jobs.middlewares.Something': 543,
-#}
-
-# Enable or disable downloader middlewares
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
    'juniorguru.sync.jobs.monitoring.BackupResponseMiddleware': 530,
    'juniorguru.sync.jobs.proxy.ScrapingProxyMiddleware': 555,
 }
 
-# Enable or disable extensions
-# See https://docs.scrapy.org/en/latest/topics/extensions.html
 EXTENSIONS = {
    'juniorguru.sync.jobs.monitoring.MonitoringExtension': 100,
 }
 
-# Configure item pipelines
-# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
    'juniorguru.sync.jobs.pipelines.identifier.Pipeline': 1,
    'juniorguru.sync.jobs.pipelines.required_fields_filter.Pipeline': 50,
@@ -101,13 +61,9 @@ JUNIORGURU_ITEM_PIPELINES = {
    },
 }
 
-# Media Pipeline
-# See https://docs.scrapy.org/en/latest/topics/media-pipeline.html
 MEDIA_ALLOW_REDIRECTS = True
 IMAGES_STORE = 'juniorguru/images'
 
-# Feeds
-today = date.today()
 FEEDS = {
     f'juniorguru/data/jobs/{today:%Y}/{today:%m}/{today:%d}.jsonl': {
         'format': 'jsonlines',
@@ -119,21 +75,8 @@ FEEDS = {
     },
 }
 
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
 AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
 
-# Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
 HTTPCACHE_ENABLED = True
 HTTPCACHE_EXPIRATION_SECS = 0
 HTTPCACHE_DIR = 'http_cache'
