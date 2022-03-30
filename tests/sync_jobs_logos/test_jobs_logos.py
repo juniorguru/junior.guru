74a2d38a1e11b5f724f9574028f0bb57e1e0258e
Honza Javorek <mail@honzajavorek.cz>
2022-03-30 11:39:25 +0200

    add a few tests of jobs-logos

--- tests/sync_jobs_logos/test_jobs_logos.py
+++ tests/sync_jobs_logos/test_jobs_logos.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from PIL import Image
 
-from juniorguru.sync.jobs_logos import convert_image, SIZE_PX
+from juniorguru.sync.jobs_logos import convert_image, unique, sort_key, SIZE_PX
 
 
 FIXTURES_DIR = Path(__file__).parent
@@ -18,3 +18,106 @@ def test_company_logo_convert_image_has_expected_size():
 
     assert image.width == SIZE_PX
     assert image.height == SIZE_PX
+
+
+def test_sort_key_prefers_successfully_downloaded_images():
+    key1 = sort_key(dict(image_path=None,
+                         type='logo',
+                         orig_width=SIZE_PX,
+                         orig_height=SIZE_PX))
+    key2 = sort_key(dict(image_path='path/to/somewhere.png',
+                         type='logo',
+                         orig_width=SIZE_PX,
+                         orig_height=SIZE_PX))
+
+    assert key2 < key1
+
+
+def test_sort_key_prefers_successfully_downloaded_images_even_if_icon():
+    key1 = sort_key(dict(image_path=None,
+                         type='logo',
+                         orig_width=SIZE_PX,
+                         orig_height=SIZE_PX))
+    key2 = sort_key(dict(image_path='path/to/somewhere.png',
+                         type='icon',
+                         orig_width=SIZE_PX,
+                         orig_height=SIZE_PX))
+
+    assert key2 < key1
+
+
+def test_sort_key_prefers_logo_over_icon():
+    key1 = sort_key(dict(image_path='path/to/somewhere.png',
+                         type='icon',
+                         orig_width=SIZE_PX,
+                         orig_height=SIZE_PX))
+    key2 = sort_key(dict(image_path='path/to/somewhere.png',
+                         type='logo',
+                         orig_width=SIZE_PX,
+                         orig_height=SIZE_PX))
+
+    assert key2 < key1
+
+
+def test_sort_key_treats_small_images_like_squares():
+    key1 = sort_key(dict(image_path='path/to/somewhere.png',
+                         type='icon',
+                         orig_width=1,
+                         orig_height=16))
+    key2 = sort_key(dict(image_path='path/to/somewhere.png',
+                         type='icon',
+                         orig_width=4,
+                         orig_height=4))
+
+    assert key1 == key2
+
+
+def test_sort_key_prefers_squares():
+    key1 = sort_key(dict(image_path='path/to/somewhere.png',
+                         type='logo',
+                         orig_width=100,
+                         orig_height=1600))
+    key2 = sort_key(dict(image_path='path/to/somewhere.png',
+                         type='logo',
+                         orig_width=400,
+                         orig_height=400))
+
+    assert key2 < key1
+
+
+def test_sort_key_prefers_square_like_rectangles():
+    key1 = sort_key(dict(image_path='path/to/somewhere.png',
+                         type='logo',
+                         orig_width=100,
+                         orig_height=1600))
+    key2 = sort_key(dict(image_path='path/to/somewhere.png',
+                         type='logo',
+                         orig_width=305,
+                         orig_height=300))
+
+    assert key2 < key1
+
+
+def test_sort_key_prefers_larger_images():
+    key1 = sort_key(dict(image_path='path/to/somewhere.png',
+                         type='logo',
+                         orig_width=500,
+                         orig_height=500))
+    key2 = sort_key(dict(image_path='path/to/somewhere.png',
+                         type='logo',
+                         orig_width=1000,
+                         orig_height=1000))
+
+    assert key2 < key1
+
+
+def test_unique():
+    assert sorted(unique([1, 3, 5, 3, 6, 0, 1])) == sorted([1, 3, 5, 6, 0])
+
+
+def test_unique_returns_list():
+    assert isinstance(unique([1, 3, 5, 3]), list)
+
+
+def test_unique_skips_none():
+    assert sorted(unique([1, 3, None, 5, 3, None, 6, 0, 1])) == sorted([1, 3, 5, 6, 0])
