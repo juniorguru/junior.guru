8a4cbf3cca3783b2025b6c2ee48df2579b9c06bf
Honza Javorek <mail@honzajavorek.cz>
2022-04-07 20:20:16 +0200

    oops, forgotten file

---
+++ juniorguru/utils/winners.py
@@ -0,0 +1,36 @@
+import random
+import time
+
+from invoke import task
+
+from juniorguru.lib.club import emoji_name, run_discord_task
+
+
+@task(name='winners')
+def main(context, message_url, winners_count):
+    run_discord_task('juniorguru.utils.winners.discord_task', message_url, winners_count)
+
+
+async def discord_task(client, message_url, winners_count):
+    message_url_parts = message_url.split('/')
+    channel_id = int(message_url_parts[-2])
+    channel = await client.fetch_channel(channel_id)
+    print(f'Message is in channel #{channel.name}')
+
+    message_id = int(message_url_parts[-1])
+    message = await channel.fetch_message(message_id)
+    print(f'Message has been posted by {message.author.display_name} at {message.created_at.isoformat()}+00:00')
+
+    reactions_count = sum([reaction.count for reaction in message.reactions])
+    print(f'Message has {reactions_count} reactions in total')
+    users = set()
+    for reaction in message.reactions:
+        async for user in reaction.users():
+            print(f'User {user.display_name} reacted with: {emoji_name(reaction.emoji)}')
+            users.add(user)
+    print(f'{len(users)} users reacted')
+    for i in range(1, 6):
+        print('The winners are…')
+        time.sleep(1 * i)
+    for user in random.sample(users, winners_count):
+        print(f'🏆 {user.display_name} (id #{user.id})')
