a5c7bb2608d96cda386ce629d2a860b7175927c0
Honza Javorek <mail@honzajavorek.cz>
2022-05-30 18:56:57 +0200

    fix the winners script

--- juniorguru/utils/winners.py
+++ juniorguru/utils/winners.py
@@ -3,34 +3,48 @@ import time
 
 from invoke import task
 
+from juniorguru.lib import loggers
 from juniorguru.lib.club import emoji_name, run_discord_task
+
+
+ROLE = 836959652100702248
+
+
+logger = loggers.get(__name__)
 
 
 @task(name='winners')
 def main(context, message_url, winners_count):
-    run_discord_task('juniorguru.utils.winners.discord_task', message_url, winners_count)
+    run_discord_task('juniorguru.utils.winners.discord_task', message_url, int(winners_count))
 
 
 async def discord_task(client, message_url, winners_count):
+    roles = await client.juniorguru_guild.fetch_roles()
+    role = [role for role in roles if role.id == ROLE][0]
+    logger.info(f"Limiting winners to only those with role '{role.name}'")
+
     message_url_parts = message_url.split('/')
     channel_id = int(message_url_parts[-2])
     channel = await client.fetch_channel(channel_id)
-    print(f'Message is in channel #{channel.name}')
+    logger.info(f'Message is in channel #{channel.name}')
 
     message_id = int(message_url_parts[-1])
     message = await channel.fetch_message(message_id)
-    print(f'Message has been posted by {message.author.display_name} at {message.created_at.isoformat()}+00:00')
+    logger.info(f'Message has been posted by {message.author.display_name} at {message.created_at.isoformat()}+00:00')
 
     reactions_count = sum([reaction.count for reaction in message.reactions])
-    print(f'Message has {reactions_count} reactions in total')
+    logger.info(f'Message has {reactions_count} reactions in total')
     users = set()
     for reaction in message.reactions:
         async for user in reaction.users():
-            print(f'User {user.display_name} reacted with: {emoji_name(reaction.emoji)}')
-            users.add(user)
-    print(f'{len(users)} users reacted')
+            logger.info(f'User {user.display_name} reacted with: {emoji_name(reaction.emoji)}')
+            if role in user.roles:
+                users.add(user)
+            else:
+                logger.warning(f"User {user.display_name} doesn't have role '{role.name}' and can't win!")
+    logger.info(f'{len(users)} users reacted')
     for i in range(1, 6):
-        print('The winners are…')
+        logger.info('The winners are…')
         time.sleep(1 * i)
     for user in random.sample(users, winners_count):
-        print(f'🏆 {user.display_name} (id #{user.id})')
+        logger.info(f'🏆 {user.display_name} (id #{user.id})')
