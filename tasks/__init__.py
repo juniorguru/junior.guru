8a7ecabcabc10c8b5d6a9bc93af75fa411961348
Honza Javorek <mail@honzajavorek.cz>
2022-07-21 23:35:08 +0200

    cancel previous build

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,5 +1,10 @@
+import os
+import sys
+from datetime import datetime, timedelta
+
 import pytest
 from invoke import Collection, Exit, task
+import requests
 
 from juniorguru.utils import checks
 from juniorguru.utils.screenshots import main as screenshots
@@ -29,4 +34,40 @@ def format(context):
     context.run('poetry run isort .')
 
 
-namespace = Collection(test, lint, format, screenshots, winners, students, participant, sync, web, checks)
+@task()
+def cancel_previous_builds(context):
+    if not os.getenv('CIRCLECI'):
+        print('Not CircleCI', file=sys.stderr)
+        raise Exit(code=1)
+
+    branch = os.environ['CIRCLE_BRANCH']
+    if not branch:
+        print('Unable to get CircleCI branch', file=sys.stderr)
+        raise Exit(code=1)
+
+    response = requests.get('https://circleci.com/api/v2/project/github/honzajavorek/junior.guru/pipeline',
+                            params={'branch': branch},
+                            headers={'Circle-Token': os.environ['CIRCLECI_API_KEY']})
+    response.raise_for_status()
+
+    day_ago = datetime.utcnow() - timedelta(days=1)
+    pipelines = [pipeline for pipeline in response.json()['items']
+                 if datetime.fromisoformat(pipeline['created_at'].rstrip('Z')) > day_ago]
+
+    for pipeline in pipelines:
+        response = requests.get(f"https://circleci.com/api/v2/pipeline/{pipeline['id']}/workflow",
+                                headers={'Circle-Token': os.environ['CIRCLECI_API_KEY']})
+        response.raise_for_status()
+
+        workflows = [workflow for workflow in response.json()['items']
+                     if workflow['id'] != os.environ['CIRCLE_WORKFLOW_ID'] and workflow['status'] == 'running']
+
+        for workflow in workflows:
+            print(f"Canceling https://app.circleci.com/pipelines/github/honzajavorek/junior.guru/{workflow['pipeline_number']}/workflows/{workflow['id']}", file=sys.stderr)
+            response = requests.post(f"https://circleci.com/api/v2/workflow/{workflow['id']}/cancel",
+                                     headers={'Circle-Token': os.environ['CIRCLECI_API_KEY']})
+            response.raise_for_status()
+
+
+namespace = Collection(test, lint, format, screenshots, winners, students, participant, cancel_previous_builds,
+                       sync, web, checks)
