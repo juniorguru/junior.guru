8837ea21b89790eed0d771a7d0df2e84cf9f6d13
Honza Javorek <mail@honzajavorek.cz>
2022-08-03 23:36:59 +0200

    fix build cancelling

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -50,23 +50,28 @@ def cancel_previous_builds(context):
                             headers={'Circle-Token': os.environ['CIRCLECI_API_KEY']})
     response.raise_for_status()
 
-    day_ago = datetime.utcnow() - timedelta(days=1)
+    day_ago = datetime.utcnow() - timedelta(days=3)
     pipelines = [pipeline for pipeline in response.json()['items']
                  if datetime.fromisoformat(pipeline['created_at'].rstrip('Z')) > day_ago]
 
+    running_workflows = {}
     for pipeline in pipelines:
         response = requests.get(f"https://circleci.com/api/v2/pipeline/{pipeline['id']}/workflow",
                                 headers={'Circle-Token': os.environ['CIRCLECI_API_KEY']})
         response.raise_for_status()
+        running_workflows.update((workflow['id'], workflow) for workflow in response.json()['items']
+                                 if workflow['status'] == 'running')
 
-        workflows = [workflow for workflow in response.json()['items']
-                     if workflow['id'] != os.environ['CIRCLE_WORKFLOW_ID'] and workflow['status'] == 'running']
+    current_workflow = running_workflows.pop(os.environ['CIRCLE_WORKFLOW_ID'])
+    created_at = datetime.fromisoformat(current_workflow['created_at'].rstrip('Z'))
+    workflows_to_cancel = [workflow for workflow in running_workflows.values() if
+                           datetime.fromisoformat(workflow['created_at'].rstrip('Z')) < created_at]
 
-        for workflow in workflows:
-            print(f"Canceling https://app.circleci.com/pipelines/github/honzajavorek/junior.guru/{workflow['pipeline_number']}/workflows/{workflow['id']}", file=sys.stderr)
-            response = requests.post(f"https://circleci.com/api/v2/workflow/{workflow['id']}/cancel",
-                                     headers={'Circle-Token': os.environ['CIRCLECI_API_KEY']})
-            response.raise_for_status()
+    for workflow in workflows_to_cancel:
+        print(f"Canceling https://app.circleci.com/pipelines/github/honzajavorek/junior.guru/{workflow['pipeline_number']}/workflows/{workflow['id']}", file=sys.stderr)
+        response = requests.post(f"https://circleci.com/api/v2/workflow/{workflow['id']}/cancel",
+                                    headers={'Circle-Token': os.environ['CIRCLECI_API_KEY']})
+        response.raise_for_status()
 
 
 namespace = Collection(test, lint, format, screenshots, winners, students, participant, cancel_previous_builds,
