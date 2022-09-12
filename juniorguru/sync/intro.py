33603039573ecf604b40b0a7271c9ed9afa17990
Honza Javorek <mail@honzajavorek.cz>
2022-09-12 13:30:06 +0200

    perhaps this is the right way to 'suppress' the embeds

--- juniorguru/sync/intro.py
+++ juniorguru/sync/intro.py
@@ -117,10 +117,10 @@ async def welcome(channel, message, moderators):
                 welcome_discord_message = list(filter(is_welcome_message, discord_messages))[0]
                 logger_m.debug(f"Welcome message already exists, updating: #{welcome_discord_message.id}")
                 if welcome_discord_message.embeds or welcome_discord_message.content != content:
-                    await welcome_discord_message.edit(content=content, embed=None)
+                    await welcome_discord_message.edit(content=content, suppress=True)
             except IndexError:
                 logger_m.debug("Sending welcome message")
-                await thread.send(content=content, embed=None)
+                await thread.send(content=content, suppress=True)
 
             logger_m.debug("Analyzing if all moderators are involved")
             thread_members_ids = [member.id for member in (thread.members or await thread.fetch_members())]
