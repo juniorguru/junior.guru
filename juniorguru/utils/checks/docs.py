8e8316751846951d77b04b3af29de82e8823b354
Honza Javorek <mail@honzajavorek.cz>
2022-10-04 14:51:32 +0200

    fix the docs checking script

--- juniorguru/utils/checks/docs.py
+++ juniorguru/utils/checks/docs.py
@@ -12,59 +12,99 @@ logger = loggers.get(__name__)
 PUBLIC_DIR = Path('public')
 
 
+class EmailLinkError(ValueError):
+    pass
+
+
+class ExternalLinkError(ValueError):
+    pass
+
+
+class StaticFileLinkError(ValueError):
+    pass
+
+
 @task(name='docs')
 def main(context):
     links = []
     targets = set()
-    images = set()
+    static = set()
 
-    for path in PUBLIC_DIR.glob('**/*.html'):
-        logger.info(f'Reading {path}')
-        doc_name = f'/{path.relative_to(PUBLIC_DIR)}'
-        doc_name = doc_name[:-10] if doc_name.endswith('index.html') else doc_name
+    for doc_path in PUBLIC_DIR.glob('**/*.html'):
+        logger.info(f'Reading {doc_path}')
+        doc_name = get_doc_name(doc_path)
         targets.add(doc_name)
 
-        html_tree = html.fromstring(path.read_bytes())
+        html_tree = html.fromstring(doc_path.read_bytes())
         for element in html_tree.cssselect('a[name]'):
             targets.add(f"{doc_name}#{element.get('name')}")
         for element in html_tree.cssselect('*[id]'):
             targets.add(f"{doc_name}#{element.get('id')}")
         for element in html_tree.cssselect('a[href]'):
             href = element.get('href')
-            if href.startswith(('http', 'mailto')):
-                continue
-            if href.startswith('#'):
-                href = f'{doc_name}{href}'
-            if not href.startswith(('.', '/')):
-                href = f'./{href}'
-            if href.startswith('.'):
-                href = f'/{path.parent.joinpath(href).resolve().relative_to(PUBLIC_DIR.absolute())}'
-            if not href.endswith('/') and '#' not in href:
-                href = f'{href}/'
-            if href == '/./':
-                href = '/'
-            links.append((doc_name, href))
+            try:
+                links.append((doc_name, normalize_link(doc_path, href)))
+            except StaticFileLinkError:
+                static.add((doc_name, normalize_static_link(doc_path, href)))
+            except ValueError:
+                logger.debug(f'Skipping: {href}')
         for element in html_tree.cssselect('img[src]'):
             src = element.get('data-src', element.get('src'))
-            if src.startswith(('http', 'mailto')):
-                continue
-            if src.startswith('/'):
-                src = src.lstrip('/')
-            elif src.startswith('.'):
-                src = f'{path.parent.joinpath(src).resolve().relative_to(PUBLIC_DIR.absolute())}'
-            images.add((doc_name, src))
+            try:
+                static.add((doc_name, normalize_static_link(doc_path, src)))
+            except ValueError:
+                logger.debug(f'Skipping: {src}')
 
     broken = False
     for doc_name, link in links:
         if link not in targets:
             logger.error(f'Broken link! {doc_name} links to {link}')
             broken = True
-    for doc_name, image_path in images:
-        image_path = PUBLIC_DIR / image_path
-        if not image_path.exists():
-            logger.error(f'Broken image! {doc_name} links to {image_path}')
+    for doc_name, static_path in static:
+        static_path = PUBLIC_DIR / static_path
+        if not static_path.exists():
+            logger.error(f'Broken static file! {doc_name} links to {static_path}')
             broken = True
     if broken:
         raise Exit(code=1)
     else:
-        logger.info(f'Checked {len(links)} links, {len(targets)} targets, {len(images)} images. All good!')
+        logger.info(f'Checked {len(links)} links, {len(targets)} targets, {len(static)} static files. All good!')
+
+
+def get_doc_name(doc_path):
+    doc_name = f'/{doc_path.relative_to(PUBLIC_DIR)}'
+    if doc_name.endswith('index.html'):
+        return doc_name[:-10]
+    return doc_name
+
+
+def normalize_link(doc_path, link):
+    if link.startswith('http'):
+        raise ExternalLinkError(link)
+    if link.startswith('mailto'):
+        raise EmailLinkError(link)
+    if Path(link).suffix not in ('', '.html'):
+        raise StaticFileLinkError(link)
+    if link.startswith('#'):
+        link = f'{get_doc_name(doc_path)}{link}'
+    if not link.startswith(('.', '/')):
+        link = f'./{link}'
+    if link.startswith('.'):
+        link = f'/{doc_path.parent.joinpath(link).resolve().relative_to(PUBLIC_DIR.absolute())}'
+    if not link.endswith('/') and '#' not in link:
+        link = f'{link}/'
+    if link == '/./':
+        link = '/'
+    return link
+
+
+def normalize_static_link(doc_path, link):
+    if link.startswith('http'):
+        raise ExternalLinkError(link)
+    if link.startswith('mailto'):
+        raise EmailLinkError(link)
+    if link.startswith('/'):
+        return link.lstrip('/')
+    if link.startswith('.'):
+        return f'{doc_path.parent.joinpath(link).resolve().relative_to(PUBLIC_DIR.absolute())}'
+    return link
