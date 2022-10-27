2ca28b3348065b622b581fd1f30d66b50239e418
Honza Javorek <mail@honzajavorek.cz>
2022-10-27 21:22:36 +0200

    refactoring to hell

--- juniorguru/cli/__init__.py
+++ juniorguru/cli/__init__.py
@@ -1,12 +1,11 @@
 import click
 
-from juniorguru.lib.cli import BaseGroup
 from juniorguru.cli.dev import main as dev
 from juniorguru.cli import (cancel_previous_builds, check_docs, check_links, data,
                             participant, screenshots, students, web, winners, sync)
 
 
-class Group(BaseGroup):
+class MainGroup(click.MultiCommand):
     def load_dynamic_commands(self):
         for module in [cancel_previous_builds,
                        check_docs,
@@ -20,8 +19,14 @@ class Group(BaseGroup):
                        sync]:
             yield module.__name__.split('.')[-1].replace('_', '-'), module.main
 
+    def list_commands(self, context):
+        return sorted([name for name, command in self.load_dynamic_commands()])
 
-@click.group(cls=Group)
+    def get_command(self, context, name):
+        return dict(self.load_dynamic_commands())[name]
+
+
+@click.group(cls=MainGroup)
 def subcommands():
     pass
 
--- juniorguru/sync/stories.py
+++ juniorguru/sync/stories.py
@@ -1,10 +1,9 @@
 from pathlib import Path
 
-import click
 from strictyaml import Datetime, Map, Seq, Str, Url, load
 
 from juniorguru.lib import loggers
-from juniorguru.cli.sync import ChainCommand
+from juniorguru.cli.sync import subcommands as cli
 from juniorguru.models.base import db
 from juniorguru.models.story import Story
 
@@ -25,7 +24,7 @@ YAML_SCHEMA = Seq(
 logger = loggers.get(__name__)
 
 
-@click.command(cls=ChainCommand)
+@cli.command()
 @db.connection_context()
 def main():
     Story.drop_table()
