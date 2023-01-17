2de080554f2873c02d02937f2bb5104b2f8fd940
Honza Javorek <mail@honzajavorek.cz>
2023-01-17 11:32:04 +0100

    implement server template backups

--- juniorguru/cli/dev.py
+++ juniorguru/cli/dev.py
@@ -1,7 +1,19 @@
 import subprocess
+from pathlib import Path
 
 import click
 import pytest
+
+from juniorguru.lib import loggers
+from juniorguru.lib.club import (DISCORD_MUTATIONS_ENABLED, run_discord_task)
+
+
+DATA_DIR = 'juniorguru/data'
+
+BACKUP_FILE = 'backup.tar.gz'
+
+
+logger = loggers.from_path(__file__)
 
 
 @click.group()
@@ -47,3 +59,33 @@ def test(pytest_args):
     code = pytest.main(list(pytest_args))
     if code:
         raise click.Abort()
+
+
+@main.command()
+@click.option('--data-dir', default=DATA_DIR, type=click.Path(path_type=Path, exists=True, file_okay=False))
+@click.option('--backup-file', default=BACKUP_FILE, type=click.Path(path_type=Path, exists=False, dir_okay=False))
+@click.option('--discord/--no-discord', default=DISCORD_MUTATIONS_ENABLED)
+@click.option('--discord-template', default='jg-backup')
+def backup(data_dir, backup_file, discord, discord_template):
+    logger['backup'].info(f'Backing up {data_dir} to {backup_file}')
+    subprocess.run(['tar', '-cvzf', backup_file, data_dir], check=True)
+    logger['backup'].info(f'Done! {backup_file.stat().st_size / 1048576:.0f} MB')
+    if discord:
+        logger['backup'].info('Backing up Discord')
+        run_discord_task('juniorguru.cli.dev.backup_discord_task', discord_template)
+    else:
+        logger['backup'].info('Discord backup not enabled')
+
+
+async def backup_discord_task(client, template_name):
+    try:
+        logger['backup'].info(f'Looking for template {template_name}')
+        template = [template for template in (await client.juniorguru_guild.templates())
+                    if template.name == template_name][0]
+    except IndexError:
+        logger['backup'].warning(f'Not found! Creating template {template_name}')
+        await client.juniorguru_guild.create_template(name=template_name)
+    else:
+        logger['backup'].info(f'Syncing template {template_name}')
+        await template.sync()
+    logger['backup'].info(f'Syncing template {template_name}')
