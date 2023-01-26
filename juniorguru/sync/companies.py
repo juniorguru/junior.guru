a6f20e6b260410535ca9f3eda3808d0c01be10d7
Honza Javorek <mail@honzajavorek.cz>
2023-01-26 18:27:40 +0100

    verify all data is correct

--- juniorguru/sync/companies.py
+++ juniorguru/sync/companies.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import click
+from strictyaml import Datetime, Map, Optional, Seq, Str, load, Bool
 
 from juniorguru.cli.sync import main as cli
 from juniorguru.lib import google_sheets, loggers
@@ -15,6 +16,24 @@ from juniorguru.models.company import Company
 logger = loggers.from_path(__file__)
 
 
+YAML_PATH = Path(__file__).parent.parent / 'data' / 'companies.yml'
+
+YAML_SCHEMA = Seq(
+    Map({
+        'name': Str(),
+        'slug': Str(),
+        'url': Str(),
+        Optional('students'): Bool(),
+        'collab': Seq(
+            Map({
+                'plan': Str(),
+                'starts_on': Datetime(),
+                Optional('expires_on'): Datetime(),
+            }),
+        ),
+    })
+)
+
 IMAGES_DIR = Path(__file__).parent.parent / 'images'
 
 POSTERS_DIR = IMAGES_DIR / 'posters-companies'
@@ -22,8 +41,6 @@ POSTERS_DIR = IMAGES_DIR / 'posters-companies'
 POSTER_WIDTH = 700
 
 POSTER_HEIGHT = 700
-
-WORKERS = 4
 
 
 @cli.sync_command()
@@ -38,6 +55,7 @@ def main(flush_posters):
     doc_key = '1TO5Yzk0-4V_RzRK5Jr9I_pF5knZsEZrNn2HKTXrHgls'
     records = google_sheets.download(google_sheets.get(doc_key, 'companies'))
 
+    logger.info('Setting up companies db table')
     Company.drop_table()
     Company.create_table()
 
@@ -54,11 +72,15 @@ def main(flush_posters):
         company.poster_path = image_path.relative_to(IMAGES_DIR)
         company.save()
 
+    # TODO
+    logger.info('Reading YAML with companies')
+    yaml_records = (record.data for record in load(YAML_PATH.read_text(), YAML_SCHEMA))
+    list(yaml_records)
+
 
 def coerce_record(record):
     data = coerce({
         r'^name$': ('name', parse_text),
-        r'^email$': ('email', parse_text),
         r'^filename$': ('logo_filename', parse_text),
         r'^handbook$': ('is_sponsoring_handbook', parse_boolean_words),
         r'^student coupon$': ('student_coupon', parse_text),
@@ -70,6 +92,8 @@ def coerce_record(record):
     }, record)
     if data.get('coupon'):
         data['slug'] = parse_slug(data['coupon'])
+    if not data.get('slug'):
+        data['slug'] = data['logo_filename'].replace('.svg', '')
     return data
 
 
