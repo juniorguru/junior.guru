29c0a450320f3598a222c11848a3bc1ec4d21273
Honza Javorek <mail@honzajavorek.cz>
2023-01-26 14:11:55 +0100

    change how pushing to github works

--- juniorguru/cli/dev.py
+++ juniorguru/cli/dev.py
@@ -47,25 +47,10 @@ def lint():
 
 
 @main.command()
-@click.option('--reset-git/--no-reset-git', default=False)
-@click.option('--push/--no-push', default=False)
-def format(reset_git, push):
+@click.argument('path', type=click.Path(exists=True, type=Path))
+def format(path):
     try:
-        if reset_git:
-            logger['format'].info('Resetting Git')
-            subprocess.run(['git', 'reset', '--hard'], check=True)
-            subprocess.run(['git', 'clean', '-f', '-d'], check=True)
-
-        logger['format'].info('Formatting code')
-        subprocess.run(['isort', '.'], check=True)
-
-        if push:
-            if subprocess.run(['git', 'diff-index', '--quiet', 'HEAD']).returncode:
-                logger['format'].info('Pushing changes')
-                subprocess.run(['git', 'commit', '-am', 'format code 💅 [skip ci]'], check=True)
-                subprocess.run(['git', 'push'], check=True)
-            else:
-                logger['format'].info('No changes to push')
+        subprocess.run(['isort', str(path)], check=True)
     except subprocess.CalledProcessError:
         raise click.Abort()
 
@@ -113,3 +98,38 @@ async def backup_discord_task(client, template_name):
 def deploy(commit_hash, build_url):
     message = f'deploy {commit_hash} 🐣 [skip ci]\n\n{build_url}'
     ghp_import('public', mesg=message, push=True, cname='junior.guru', nojekyll=True)
+
+
+@main.command()
+def reset_repo():
+    try:
+        subprocess.run(['git', 'reset', '--hard'], check=True)
+        subprocess.run(['git', 'clean', '-f', '-d'], check=True)
+    except subprocess.CalledProcessError:
+        raise click.Abort()
+
+
+@main.command()
+@click.argument('paths', nargs=-1, type=click.Path(exists=True, type=Path))
+@click.option('--message', default='save changes 🛠️')
+@click.option('--build-url', envvar='CIRCLE_BUILD_URL')
+@click.option('--skip-ci/--no-skip-ci', default=True)
+def save_changes(paths, message, build_url, skip_ci):
+    if skip_ci:
+        message += ' [skip ci]'
+    if build_url:
+        message += f'\n\n{build_url}'
+    try:
+        for path in paths:
+            logger['save-changes'].info(f'Adding path: {path}')
+            subprocess.run(['git', 'add', '-A', str(path)], check=True)
+
+        if subprocess.run(['git', 'diff', '--name-only', '--cached']).stdout:
+            logger['save-changes'].info(f'Commit message: {message!r}')
+            subprocess.run(['git', 'commit', '-m', message], check=True)
+            logger['save-changes'].info('Pushing changes')
+            subprocess.run(['git', 'push'], check=True)
+        else:
+            logger['save-changes'].warning('No changes to push')
+    except subprocess.CalledProcessError:
+        raise click.Abort()
