juniorguru/junior.guru

commit b9ca2f84dc5cf52793fee3a1b3b9b5ecc153499a

use normal formatting

juniorguru/models/club.py
@@ -171,7 +171,7 @@ class ClubMessage(BaseModel):
     @classmethod
     def count_by_month(cls, date):
         return cls.select() \
-            .where(cls.created_month == f'{date.year}-{date.month}') \
+            .where(cls.created_month == f'{date:%Y-%-d}') \
             .where(cls.channel_id.not_in(STATS_EXCLUDE_CHANNELS)) \
             .count()
 

juniorguru/sync/club_content.py
@@ -166,7 +166,7 @@ async def channel_worker(worker_no, authors, queue):
                                upvotes_count=count_upvotes(message.reactions),
                                downvotes_count=count_downvotes(message.reactions),
                                created_at=arrow.get(message.created_at).naive,
-                               created_month=f'{message.created_at.year}-{message.created_at.month}',
+                               created_month=f'{message.created_at:%Y-%-d}',
                                edited_at=(arrow.get(message.edited_at).naive if message.edited_at else None),
                                author=authors[message.author.id],
                                channel_id=channel.id,

juniorguru/image_templates/event.html
@@ -28,7 +28,7 @@
       </div>
       <div class="teaser">
         <p class="teaser-text">
-          {{ event.start_at|weekday|capitalize }} <strong>{{ event.start_at.day }}. {{ event.start_at.month }}. {{ event.start_at.year }}</strong>, začátek v <strong>{{ event.start_at|local_time }}</strong>
+          {{ event.start_at|weekday|capitalize }} <strong>{{ ':%-d.%-m.%Y'.format(event.start_at) }}</strong>, začátek v <strong>{{ event.start_at|local_time }}</strong>
           <br>
           Online pro členy <strong>junior.guru/club</strong>
         </p>

juniorguru/mkdocs/macros/macros.html
@@ -278,7 +278,7 @@
             <a href="{{ story.url }}" class="stories-link">{{ story.title }}</a>
           </h4>
           <p class="stories-details">
-            {{ story.publisher }} &mdash; {{ story.date.month }}/{{ story.date.year }}
+            {{ story.publisher }} &mdash; {{ '%-m/%Y'.format(story.date) }}
           </p>
         </div>
       </div>

juniorguru/sync/dashboard.py
@@ -132,7 +132,7 @@ def render_events():
 def format_event(event):
     return (
         f'📺 [{event.title}]({event.recording_url})\n'
-        f'{event.start_at.date().day}.{event.start_at.date().month}.{event.start_at.date().year}, {event.bio_name}\n'
+        f'{event.start_at.date():%-d.%-m.%Y}, {event.bio_name}\n'
     )
 
 

juniorguru/sync/digest.py
@@ -39,7 +39,7 @@ async def discord_task(client):
 
         if DISCORD_MUTATIONS_ENABLED:
             content = [
-                f"🔥 **{DIGEST_LIMIT} nej příspěvků za uplynulý týden (od {since_date.day}.{since_date.month}.)**",
+                f"🔥 **{DIGEST_LIMIT} nej příspěvků za uplynulý týden (od {since_date:%-d.%-m.})**",
                 "",
                 "Pokud je něco zajímavé nebo ti to pomohlo, dej tomu palec 👍, srdíčko ❤️, očička 👀, apod. Oceníš autory a pomůžeš tomu, aby se příspěvek mohl objevit i tady. Někomu, kdo nemá čas procházet všechno, co se v klubu napíše, se může tento přehled hodit.",
             ]

juniorguru/sync/jobs_emails/templates/job_metrics.html
@@ -45,7 +45,7 @@
       <b>{{ start_days }} dní</b>&nbsp;&nbsp;&nbsp;
     </td>
     <td>
-      <small>inzerát schválen {{ start_at.day }}.{{ start_at.month }}.{{ start_at.year }}</small>
+      <small>inzerát schválen {{ ':%-d.%-m.%Y'.format(start_at) }}</small>
     </td>
   </tr>
   <tr>
@@ -56,7 +56,7 @@
       <b>{{ end_days }} dní</b>&nbsp;&nbsp;&nbsp;
     </td>
     <td>
-      <small>inzerát vyprší {{ end_at.day }}.{{ end_at.month }}.{{ end_at.year }}</small>
+      <small>inzerát vyprší {{ ':%-d.%-m.%Y'.format(event.end_at) }}</small>
     </td>
   </tr>
   <tr>

juniorguru/sync/partners_intro.py
@@ -55,7 +55,7 @@ async def discord_task(client):
                 if partnership.starts_on < COMPANIES_INTRO_LAUNCH_ON and (date.today() - partnership.starts_on).days > 30:
                     content += (
                         ' 🐣 Sice to píšu jako novinku, ale ve skutečnosti klub podporují už od '
-                        f'{partnership.starts_on.day}.{partnership.starts_on.month}.{partnership.starts_on.year}. '
+                        f'{partnership.starts_on:%-d.%-m.%Y}. '
                         'Jenže tehdy jsem bylo malé kuřátko, které ještě neumělo vítat firmy.'
                     )
 

commit 8912e0694cb30e5564c1dba2b56d9d8cb1276d03

fix regression introduced in b9ca2f8

juniorguru/models/club.py
@@ -171,7 +171,7 @@ class ClubMessage(BaseModel):
     @classmethod
     def count_by_month(cls, date):
         return cls.select() \
-            .where(cls.created_month == f'{date:%Y-%-d}') \
+            .where(cls.created_month == f'{date:%Y-%d}') \
             .where(cls.channel_id.not_in(STATS_EXCLUDE_CHANNELS)) \
             .count()
 

juniorguru/sync/club_content.py
@@ -166,7 +166,7 @@ async def channel_worker(worker_no, authors, queue):
                                upvotes_count=count_upvotes(message.reactions),
                                downvotes_count=count_downvotes(message.reactions),
                                created_at=arrow.get(message.created_at).naive,
-                               created_month=f'{message.created_at:%Y-%-d}',
+                               created_month=f'{message.created_at:%Y-%m}',
                                edited_at=(arrow.get(message.edited_at).naive if message.edited_at else None),
                                author=authors[message.author.id],
                                channel_id=channel.id,