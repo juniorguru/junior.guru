f2b7e5704b91144d4addc7d196e805b7f658a209
Honza Javorek <mail@honzajavorek.cz>
2023-02-24 21:28:25 +0100

    try if forum threads can have embeds and buttons

---
+++ juniorguru/sync/jobs_test.py
@@ -0,0 +1,31 @@
+from discord import Color, Embed, ui
+
+from juniorguru.cli.sync import main as cli
+from juniorguru.lib import loggers
+from juniorguru.lib.club import run_discord_task, DISCORD_MUTATIONS_ENABLED
+from juniorguru.models.base import db
+
+
+logger = loggers.from_path(__file__)
+
+
+JOBS_CHANNEL = 1078773263385251860
+
+
+@cli.sync_command()
+def main():
+    run_discord_task('juniorguru.sync.jobs_test.discord_task')
+
+
+@db.connection_context()
+async def discord_task(client):
+    channel = await client.juniorguru_guild.fetch_channel(JOBS_CHANNEL)
+    if DISCORD_MUTATIONS_ENABLED:
+        await channel.create_thread('Pokus', 'Tralala',
+                                    embed=Embed(title='Embed', color=Color.orange(),
+                                                description='Fuuuuuha'),
+                                    view=ui.View(ui.Button(emoji='👋',
+                                                label='Oujé',
+                                                url='https://junior.guru/')))
+    else:
+        logger.warning('Discord mutations not enabled')
