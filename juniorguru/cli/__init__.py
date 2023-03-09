d6ebc80b499ebf9f4c495de9cef1fb13736ad91b
Honza Javorek <mail@honzajavorek.cz>
2023-03-09 19:56:23 +0100

    add bot check

--- juniorguru/cli/__init__.py
+++ juniorguru/cli/__init__.py
@@ -1,7 +1,7 @@
 import click
 
 from juniorguru.cli import (cancel_previous_builds, check_docs, check_links, data,
-                            participant, screenshots, students, sync, web, winners)
+                            participant, screenshots, students, sync, web, winners, check_bot)
 from juniorguru.cli.dev import main as dev
 from juniorguru.lib.cli import load_command
 
@@ -10,6 +10,7 @@ subcommands = click.Group(commands=dict(map(load_command, [
     cancel_previous_builds,
     check_docs,
     check_links,
+    check_bot,
     web,
     data,
     participant,
