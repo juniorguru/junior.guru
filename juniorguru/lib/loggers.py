bd026206b7e5560e0602afc62758cb9203fca9b4
Honza Javorek <mail@honzajavorek.cz>
2023-03-29 15:26:12 +0200

    don't log so much

--- juniorguru/lib/loggers.py
+++ juniorguru/lib/loggers.py
@@ -24,6 +24,7 @@ MUTED_LOGGERS = [
     'protego',
     'juniorguru.lib.discord_sync',
     'juniorguru.sync.club_content.store',
+    'juniorguru.lib.mutations.force_allow',
     'juniorguru.lib.discord_club.is_message_over_period_ago',
     'juniorguru.lib.discord_club.is_message_older_than',
     'juniorguru.lib.images',
