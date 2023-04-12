76625fad2fe9aacd9d4a182e5453bfe27ce5d007
Honza Javorek <mail@honzajavorek.cz>
2023-04-12 14:41:49 +0200

    test default envs

--- tests/test_cli_sync.py
+++ tests/test_cli_sync.py
@@ -1,4 +1,4 @@
-from juniorguru.cli.sync import get_parallel_chains
+from juniorguru.cli.sync import get_parallel_chains, default_from_env
 
 
 def test_get_parallel_chains():
@@ -47,3 +47,31 @@ def test_get_parallel_chains_exclude():
                     'f': ['d']}
 
     assert get_parallel_chains(dependencies, exclude=['a']) == [['b'], ['c', 'd', 'e', 'f']]
+
+
+def test_default_from_env(monkeypatch):
+    monkeypatch.setenv('FOO', 'something')
+    env_reader = default_from_env('FOO')
+
+    assert env_reader() == 'something'
+
+
+def test_default_from_env_default(monkeypatch):
+    monkeypatch.delenv('FOO', raising=False)
+    env_reader = default_from_env('FOO', default='something')
+
+    assert env_reader() == 'something'
+
+
+def test_default_from_env_type(monkeypatch):
+    monkeypatch.setenv('FOO', '123')
+    env_reader = default_from_env('FOO', type=int)
+
+    assert env_reader() == 123
+
+
+def test_default_from_env_type_default(monkeypatch):
+    monkeypatch.delenv('FOO', raising=False)
+    env_reader = default_from_env('FOO', default=123, type=int)
+
+    assert env_reader() == 123
