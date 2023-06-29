a526a1e0c3bd52157207957fa3bc9036b9df9518
Honza Javorek <mail@honzajavorek.cz>
2023-06-29 13:40:55 +0200

    fuck it

--- juniorguru/sync/meetups.py
+++ juniorguru/sync/meetups.py
@@ -1,5 +1,5 @@
-import ics
-import requests
+# import ics
+# import requests
 
 from juniorguru.cli.sync import main as cli
 from juniorguru.lib import loggers
@@ -12,13 +12,22 @@ FEEDS = [
 ]
 
 
+# https://www.meetup.com/api/general/#graphQl-introduction
+# https://pypi.org/project/meetup-api/
+
+
 logger = loggers.from_path(__file__)
 
 
-@cli.sync_command()
+@cli.sync_command()  # TODO waiting until meetup.com decides to approve my oauth client :(
 def main():
-    for feed_url in FEEDS:
-        logger.info(f'Fetching {feed_url}')
-        calendar = ics.Calendar(requests.get(feed_url).text)
-        for event in calendar.events:
-            print(event)
+    pass
+    # for feed_url in FEEDS:
+    #     logger.info(f'Fetching {feed_url}')
+    #     response = requests.get(feed_url, headers={'User-Agent': 'JuniorGuruBot (+https://junior.guru)'})
+    #     response.raise_for_status()
+    #     calendar_text = response.text.strip()
+    #     if calendar_text:
+    #         calendar = ics.Calendar(calendar_text)
+    #         for event in calendar.events:
+    #             print(event)
