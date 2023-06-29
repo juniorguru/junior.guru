4dbe6d32410e0e6057137990e312323198db969a
Kuře <kure@junior.guru>
2023-06-29 02:11:29 +0000

    format code 💅 [skip ci]
    https://circleci.com/gh/honzajavorek/junior.guru/40182

--- juniorguru/sync/interviews_tips.py
+++ juniorguru/sync/interviews_tips.py
@@ -1,11 +1,12 @@
 from datetime import timedelta
 
-from discord import Color, Embed
 import emoji
+from discord import Color, Embed
 
 from juniorguru.cli.sync import main as cli
 from juniorguru.lib import discord_sync, loggers
-from juniorguru.lib.discord_club import ClubChannelID, ClubMemberID, is_message_over_period_ago
+from juniorguru.lib.discord_club import (ClubChannelID, ClubMemberID,
+                                         is_message_over_period_ago)
 from juniorguru.lib.mutations import mutating_discord
 from juniorguru.models.base import db
 from juniorguru.models.club import ClubMessage
