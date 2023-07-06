22fbf20287ed7be5ce09927e7b3ccd623a37a9e5
Honza Javorek <mail@honzajavorek.cz>
2023-07-06 16:54:48 +0200

    rename format to tidy up

--- juniorguru/cli/__init__.py
+++ juniorguru/cli/__init__.py
@@ -1,8 +1,8 @@
 import click
 
 from juniorguru.cli import (backup, cancel_previous_builds, check_bot, check_docs,
-                            check_links, data, format, notes, screenshots,
-                            students, sync, web, winners)
+                            check_links, data, notes, screenshots,
+                            students, sync, tidy, web, winners)
 from juniorguru.cli.dev import main as dev
 from juniorguru.lib.cli import load_command
 
@@ -15,7 +15,7 @@ subcommands = click.Group(commands=dict(map(load_command, [
     check_bot,
     web,
     data,
-    format,
+    tidy,
     screenshots,
     notes,
     winners,
