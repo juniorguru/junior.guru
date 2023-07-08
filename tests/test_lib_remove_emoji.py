667d738280a4d51957fc4373b7bd77865e3219a4
Honza Javorek <mail@honzajavorek.cz>
2023-07-08 22:09:47 +0200

    fix tests

--- tests/test_lib_remove_emoji.py
+++ tests/test_lib_remove_emoji.py
@@ -3,7 +3,7 @@ import pytest
 from juniorguru.lib.remove_emoji import remove_emoji
 
 
-@pytest.mark.parametrize('title', [
+@pytest.mark.parametrize('text', [
     '❗Kurz Programátor www aplikací❗',
     '🦸🏻 Kurz Programátor www aplikací 🦸🏻',
     '  🦸🏻 Kurz Programátor www aplikací 🦸🏻  ',
@@ -13,10 +13,10 @@ def test_remove_emoji(text):
     assert remove_emoji(text) == 'Kurz Programátor www aplikací'
 
 
-@pytest.mark.parametrize('title', [
+@pytest.mark.parametrize('text', [
     '\u200dQA Engineer/Tester',
     '  \u200dQA Engineer/Tester',
     '\u200d  QA Engineer/Tester',
 ])
 def test_remove_emoji_zero_width_joiner(text):
-    assert text == 'QA Engineer/Tester'
+    assert remove_emoji(text) == 'QA Engineer/Tester'
