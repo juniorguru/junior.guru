01686d0e3bb53360b58cb6840854b1f50c0d12f5
Honza Javorek <mail@honzajavorek.cz>
2023-07-20 15:39:20 +0200

    merge with the subscription script

--- juniorguru/sync/subscriptions/__init__.py
+++ juniorguru/sync/subscriptions/__init__.py
@@ -9,11 +9,11 @@ import click
 from juniorguru.cli.sync import main as cli
 from juniorguru.lib import loggers
 from juniorguru.lib.coupons import parse_coupon
-from juniorguru.lib.memberful import Memberful
+from juniorguru.lib.memberful import Memberful, MemberfulCSV
 from juniorguru.models.base import db
 from juniorguru.models.feminine_name import FeminineName
 from juniorguru.models.partner import Partner
-from juniorguru.models.subscription import SubscriptionActivity, SubscriptionType
+from juniorguru.models.subscription import SubscriptionActivity, SubscriptionCancellation, SubscriptionOrigin, SubscriptionReferrer, SubscriptionType
 
 
 ACTIVITIES_GQL_PATH = Path(__file__).parent / 'activities.gql'
@@ -52,8 +52,9 @@ def main(context, clear_cache):
                           clear_cache=clear_cache)
 
     logger.info('Preparing')
-    SubscriptionActivity.drop_table()
-    SubscriptionActivity.create_table()
+    tables = [SubscriptionActivity, SubscriptionReferrer, SubscriptionOrigin, SubscriptionCancellation]
+    db.drop_tables(tables)
+    db.create_tables(tables)
 
     subscripton_types_mapping = {
         **{parse_coupon(coupon)['name']: SubscriptionType.PARTNER
@@ -66,7 +67,7 @@ def main(context, clear_cache):
     def has_feminine_name(name) -> bool:
         return FeminineName.is_feminine(name.strip())
 
-    logger.info('Fetching activities from Memberful')
+    logger.info('Fetching activities from Memberful API')
     query = ACTIVITIES_GQL_PATH.read_text()
     activities = itertools.chain.from_iterable(memberful.get_nodes(query, dict(type=type), delay=0.2)
                                                for type in ACTIVITY_TYPES_MAPPING)
@@ -81,7 +82,7 @@ def main(context, clear_cache):
                                      happened_at=arrow.get(activity['createdAt']).naive,
                                      type=ACTIVITY_TYPES_MAPPING[activity['type']])
 
-    logger.info('Fetching subscriptions from Memberful')
+    logger.info('Fetching subscriptions from Memberful API')
     subscriptions = memberful.get_nodes(SUBSCRIPTIONS_GQL_PATH.read_text(), delay=0.2)
     for subscription in logger.progress(subscriptions):
         for activity in activities_from_subscription(subscription):
@@ -96,6 +97,34 @@ def main(context, clear_cache):
 
     SubscriptionActivity.cleanup()
     logger.info(f'Finished with {SubscriptionActivity.count()} activities')
+
+    logger.info("Fetching members data from Memberful CSV")
+    memberful = MemberfulCSV(cache_dir=context.obj['cache_dir'], clear_cache=clear_cache)
+    for csv_row in memberful.download_csv(dict(type='MembersCsvExport', filter='all')):
+        referrer = csv_row['Referrer'] or None
+        if referrer:
+            SubscriptionReferrer.create(account_id=csv_row['Memberful ID'],
+                                        name=csv_row['Full Name'],
+                                        email=csv_row['Email'],
+                                        referrer=referrer)
+        origin = csv_row['Jak ses dozvěděl(a) o junior.guru?'] or None
+        if origin:
+            SubscriptionOrigin.create(account_id=csv_row['Memberful ID'],
+                                      name=csv_row['Full Name'],
+                                      email=csv_row['Email'],
+                                      origin=origin)
+
+    logger.info("Fetching cancellations data from Memberful CSV")
+    csv_rows = itertools.chain(memberful.download_csv(dict(type='CancellationsCsvExport', filter='all')),
+                               memberful.download_csv(dict(type='CancellationsCsvExport', scope='completed', filter='all')))
+    for csv_row in csv_rows:
+        reason = csv_row['Reason'] or None
+        feedback = csv_row['Feedback'] or None
+        if reason:
+            SubscriptionCancellation.create(name=csv_row['Name'],
+                                            email=csv_row['Email'],
+                                            reason=reason,
+                                            feedback=feedback)
 
 
 def activities_from_subscription(subscription: dict) -> Generator[dict, None, None]:
