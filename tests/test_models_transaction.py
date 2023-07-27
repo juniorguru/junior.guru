88e0c72adf13c2c0ec25d22035f1ee379b946599
Honza Javorek <mail@honzajavorek.cz>
2023-07-27 22:26:29 +0200

    fix tests

--- tests/test_models_transaction.py
+++ tests/test_models_transaction.py
@@ -12,8 +12,9 @@ def test_db():
     yield from prepare_test_db([Transaction])
 
 
-def create_transaction(**kwargs):
+def create_transaction(id, **kwargs):
     return Transaction.create(
+        id=id,
         happened_on=kwargs.get('happened_on', date.today() - timedelta(days=3)),
         category=kwargs.get('category', 'abcd'),
         amount=kwargs.get('amount', 1038)
@@ -21,9 +22,9 @@ def create_transaction(**kwargs):
 
 
 def test_listing_sorts_from_newest_to_oldest(test_db):
-    t1 = create_transaction(happened_on=date(2021, 8, 31))
-    t2 = create_transaction(happened_on=date(2021, 8, 1))
-    t3 = create_transaction(happened_on=date(2021, 8, 15))
+    t1 = create_transaction('1', happened_on=date(2021, 8, 31))
+    t2 = create_transaction('2', happened_on=date(2021, 8, 1))
+    t3 = create_transaction('3', happened_on=date(2021, 8, 15))
 
     assert list(Transaction.listing(date(2021, 1, 1), date(2021, 12, 31))) == [t1, t3, t2]
 
