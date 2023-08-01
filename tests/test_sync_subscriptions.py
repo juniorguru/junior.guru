219c44d1aa01b70be0704ca5e9f2370aa784ea82
Honza Javorek <mail@honzajavorek.cz>
2023-08-02 00:57:46 +0200

    fix tests

--- tests/test_sync_subscriptions.py
+++ tests/test_sync_subscriptions.py
@@ -1,43 +1,8 @@
-import pytest
-
-from juniorguru.sync.subscriptions import (activities_from_subscription,
-                                           classify_marketing_survey_answer,
-                                           classify_referrer)
+from juniorguru.sync.subscriptions import activities_from_subscription
 
 
 def test_activities_from_subscription():
     assert activities_from_subscription  # TODO
-
-
-@pytest.mark.parametrize('url, expected', [
-    ('https://junior.guru/', '/'),
-    ('https://junior.guru/learn/', '/learn'),
-    ('https://junior.guru/events/?fbclid=IwAR0WkuqPqg7z6GHW-vEGgWlhgpzcT62qJkarSOrOQTsUjzxRBbSS8RGMw8I', '/events'),
-    ('https://t.co', 'twitter'),
-    ('https://t.co/RExQZc2z4V?amp=1', 'twitter'),
-    ('https://www.linkedin.com/', 'linkedin'),
-    ('https://l.facebook.com/', 'facebook'),
-    ('http://m.facebook.com', 'facebook'),
-    ('android-app://m.facebook.com/', 'facebook'),
-    ('https://honzajavorek.cz/', 'honzajavorek'),
-    ('https://honzajavorek.cz/blog/tydenni-poznamky-37-prvni-klubovy-sraz/', 'honzajavorek'),
-    ('https://www.youtube.com/', 'youtube'),
-    ('https://www.google.com', 'google'),
-    ('https://www.patreon.com/', 'other'),
-])
-def test_classify_referrer(url, expected):
-    assert classify_referrer(url) == expected
-
-
-@pytest.mark.parametrize('text, expected', [
-    ('Google', 'search'),
-    ('Youtube', 'youtube'),
-    ('Youtube videá od "Yablko"', 'yablko'),
-    ('Cez video od yablka.', 'yablko'),
-    ('bla bla', 'other'),
-])
-def test_classify_marketing_survey_answer(text, expected):
-    assert classify_marketing_survey_answer(text) == expected
 
 
 # def test_get_subscribed_periods():
