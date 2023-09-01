f57c24474245668bf29fe11f5521fb11519940ef
Honza Javorek <mail@honzajavorek.cz>
2023-09-01 14:22:26 +0200

    store exchange rates so that the app is less reliant on ČNB API

--- juniorguru/lib/yaml.py
+++ juniorguru/lib/yaml.py
@@ -1,8 +1,15 @@
 from datetime import datetime
 
 from strictyaml.scalar import ScalarValidator
+from strictyaml import Decimal as BaseDecimal
+from strictyaml.ruamel.scalarstring import DoubleQuotedScalarString
 
 
 class Date(ScalarValidator):
     def validate_scalar(self, chunk):
         return datetime.strptime(chunk.contents, "%Y-%m-%d").date()
+
+
+class Decimal(BaseDecimal):
+    def to_yaml(self, data) -> str:
+        return DoubleQuotedScalarString(str(data))
--- juniorguru/sync/exchange_rates.py
+++ juniorguru/sync/exchange_rates.py
@@ -1,11 +1,17 @@
 from datetime import date, timedelta
 from decimal import Decimal
+from operator import itemgetter
+from pathlib import Path
 from typing import Tuple
+import click
 
+import strictyaml
+from strictyaml import Seq, Map, Str
 import requests
 
 from juniorguru.cli.sync import main as cli
 from juniorguru.lib import loggers
+from juniorguru.lib.yaml import Decimal as Dec
 from juniorguru.models.base import db
 from juniorguru.models.exchange_rate import ExchangeRate
 
@@ -15,36 +21,50 @@ logger = loggers.from_path(__file__)
 
 CURRENCIES = ['EUR', 'USD']
 
+YAML_SCHEMA = Seq(
+    Map({
+        'code': Str(),
+        'rate': Dec(),
+    })
+)
+
 
 @cli.sync_command()
+@click.option('--path', 'yaml_path', default='juniorguru/data/exhange_rates.yml', type=click.Path(path_type=Path))
 @db.connection_context()
-def main():
+def main(yaml_path: Path):
     monday = get_last_monday(date.today())
     logger.info(f'Getting exchange rates for {monday}')
-    url = ('https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/'
-           'denni_kurz.txt'
-           f'?date={monday:%d.%m.%Y}')
-    response = requests.get(url, headers={'User-Agent': 'JuniorGuruBot (+https://junior.guru)'})
-    response.raise_for_status()
-
-    exchange_rates = dict(parse_exchange_rate(line) for line in parse_lines(response.text))
+    try:
+        url = ('https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/'
+            'denni_kurz.txt'
+            f'?date={monday:%d.%m.%Y}')
+        response = requests.get(url, headers={'User-Agent': 'JuniorGuruBot (+https://junior.guru)'})
+        response.raise_for_status()
+    except requests.ConnectionError:
+        logger.error('ČNB API is down, loading from YAML')
+        exchange_rates = strictyaml.load(yaml_path.read_text(), YAML_SCHEMA).data
+    else:
+        exchange_rates = [parse_exchange_rate(line) for line in parse_lines(response.text)]
+        exchange_rates = [exchange_rate for exchange_rate in exchange_rates if exchange_rate['code'] in CURRENCIES]
+        exchange_rates = sorted(exchange_rates, key=itemgetter('code'))
+        yaml_path.write_text(strictyaml.as_document(exchange_rates, YAML_SCHEMA).as_yaml())
 
     ExchangeRate.drop_table()
     ExchangeRate.create_table()
 
-    for currency in CURRENCIES:
-        logger.info(f'Saving exchange rate for {currency}: {exchange_rates[currency]}')
-        ExchangeRate.create(code=currency, rate=exchange_rates[currency])
+    for exchange_rate in exchange_rates:
+        logger.info(f"Saving exchange rate for {exchange_rate['code']}: {exchange_rate['rate']}")
+        ExchangeRate.create(code=exchange_rate['code'], rate=exchange_rate['rate'])
 
 
 def get_last_monday(today) -> date:
     return today - timedelta(days=today.weekday())
 
 
-def parse_exchange_rate(line) -> Tuple[str, Decimal]:
-    code = line.split('|')[3]
-    rate = Decimal(line.split('|')[4].replace(',', '.'))
-    return code, rate
+def parse_exchange_rate(line) -> Tuple[str, str]:
+    return dict(code=line.split('|')[3],
+                rate=Decimal(line.split('|')[4].replace(',', '.')))
 
 
 def parse_lines(text: str) -> list[str]:
