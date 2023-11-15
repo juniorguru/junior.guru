f038e247f2a3bebaf4f80247456838e3911a495c
Honza Javorek <mail@honzajavorek.cz>
2023-11-15 14:20:02 +0100

    club events archive

--- juniorguru/sync/club_events_archive.py
+++ juniorguru/sync/club_events_archive.py
@@ -1,12 +1,15 @@
+from datetime import timedelta
 from pathlib import Path
+import click
 
 from discord import Embed, File, ui
 
 from juniorguru.cli.sync import main as cli
 from juniorguru.lib import discord_sync, loggers
-from juniorguru.lib.discord_club import ClubChannelID, ClubClient
+from juniorguru.lib.discord_club import ClubClient, is_message_over_period_ago, parse_channel
 from juniorguru.lib.mutations import mutating_discord
 from juniorguru.models.base import db
+from juniorguru.models.club import ClubMessage
 from juniorguru.models.event import Event
 
 
@@ -17,14 +20,28 @@ logger = loggers.from_path(__file__)
 
 
 @cli.sync_command(dependencies=["club-content", "events"])
-def main():
-    discord_sync.run(recreate_archive)
+@click.option("--channel", "channel_id", default="events_archive", type=parse_channel)
+@click.option("--recreate-interval", 'recreate_interval_days', default=30, type=int, help='In days.')
+def main(channel_id: int, recreate_interval_days: int):
+    discord_sync.run(recreate_archive, channel_id, recreate_interval_days)
 
 
 @db.connection_context()
-async def recreate_archive(client: ClubClient):
+async def recreate_archive(client: ClubClient, channel_id: int, recreate_interval_days: int):
     events = list(Event.archive_listing())
-    channel = await client.fetch_channel(ClubChannelID.EVENTS_ARCHIVE)
+    messages = ClubMessage.channel_listing(channel_id, by_bot=True)
+    try:
+        last_message = messages[-1]
+    except IndexError:
+        logger.info("No messages in the channel")
+    else:
+        if is_message_over_period_ago(last_message, timedelta(days=recreate_interval_days)):
+            logger.info("Channel content is too old")
+        else:
+            logger.info("Channel content is recent, skipping")
+            return
+
+    channel = await client.fetch_channel(channel_id)
     with mutating_discord(channel) as proxy:
         await proxy.purge(limit=None)
     with mutating_discord(channel) as proxy:
