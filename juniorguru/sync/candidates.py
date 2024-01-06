2f8be9641bd2383e3286d1bed91dfa46a32f1f4b
Kuře <kure@junior.guru>
2024-01-06 02:11:52 +0000

    tidy up! 💅 [skip ci]
    https://circleci.com/gh/juniorguru/junior.guru/46436

--- juniorguru/sync/candidates.py
+++ juniorguru/sync/candidates.py
@@ -7,7 +7,7 @@ from juniorguru.models.candidate import Candidate
 logger = loggers.from_path(__file__)
 
 
-@cli.sync_command(dependencies=['club-content', 'roles'])
+@cli.sync_command(dependencies=["club-content", "roles"])
 @db.connection_context()
 def main():
     Candidate.drop_table()
