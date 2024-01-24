53dc283dc46ebbea0b2c73cc856163d5d66d28f0
Honza Javorek <mail@honzajavorek.cz>
2024-01-24 12:16:33 +0100

    log progress

--- juniorguru/sync/jobs_scraped/__init__.py
+++ juniorguru/sync/jobs_scraped/__init__.py
@@ -41,6 +41,25 @@ class DropItem(Exception):
     pass
 
 
+# Python 3.12 should start supporting generators for asyncio.as_completed,
+# so remove this class then
+class DisguisedGenerator:
+    def __init__(self, generator):
+        self._generator = generator
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return next(self._generator)
+
+    def __len__(self):
+        raise NotImplementedError("DisguisedGenerator doesn't support len()")
+
+    def __getitem__(self, index):
+        raise NotImplementedError("DisguisedGenerator doesn't support indexing")
+
+
 @cli.sync_command()
 @cli.pass_cache
 @async_command
@@ -66,37 +85,41 @@ async def main(cache: Cache):
 
     logger.info("Processing items")
     stats = Counter()
-    for item in logger.progress(items):
-        logger.debug(f"Item {item['url']}")
-        try:
-            item = await process_item(pipelines, item)
-        except DropItem:
-            stats["drops"] += 1
-        else:
-            stats["items"] += 1
-            logger.debug(f"Saving {item['url']}")
-            await asyncio.get_running_loop().run_in_executor(None, save_item, item)
+    for processing in logger.progress(
+        asyncio.as_completed(
+            DisguisedGenerator(process_item(pipelines, item) for item in items)
+        )
+    ):
+        stats["drops"] += 1 - (await processing)
+        stats["items"] += 1
     logger.info(f"Stats: {stats['items']} items, {stats['drops']} drops")
 
 
 async def process_item(
-    pipelines: list[Callable[..., Awaitable[dict]]],
+    pipelines: list[tuple[str, Callable[..., Awaitable[dict]]]],
     item: dict,
-) -> dict:
-    for pipeline_name, pipeline in pipelines:
-        try:
-            item = await pipeline(item)
-        except DropItem as e:
-            logger[pipeline_name].debug(f"Dropping: {e}\n{pformat(item)}")
-            raise
-        except Exception:
-            logger.error(f"Pipeline {pipeline_name!r} failed:\n{pformat(item)}")
-            raise
-    return item
+) -> int:
+    logger.debug(f"Item {item['url']}")
+    try:
+        for pipeline_name, pipeline in pipelines:
+            try:
+                item = await pipeline(item)
+            except DropItem as e:
+                logger[pipeline_name].debug(f"Dropping: {e}\n{pformat(item)}")
+                raise
+            except Exception:
+                logger.error(f"Pipeline {pipeline_name!r} failed:\n{pformat(item)}")
+                raise
+    except DropItem:
+        return 0
+
+    logger.debug(f"Saving {item['url']}")
+    await asyncio.get_running_loop().run_in_executor(None, save_item, item)
+    return 1
 
 
 @db.connection_context()
-def save_item(item: dict):
+def save_item(item: dict) -> None:
     job = ScrapedJob.from_item(item)
     try:
         job.save()
