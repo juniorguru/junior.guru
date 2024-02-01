e76e409f0b8534443a2426c5bca6461c2fbc41ea
Honza Javorek <mail@honzajavorek.cz>
2024-02-01 20:44:46 +0100

    add relevance filter

--- tests/sync_jobs_scraped_pipelines/test_blocklist_filter.py
+++ tests/sync_jobs_scraped_pipelines/test_blocklist_filter.py
@@ -5,22 +5,22 @@ from juniorguru.sync.jobs_scraped.pipelines.blocklist_filter import process
 
 
 @pytest.mark.asyncio
-async def test_blocklist_lets_junior_through():
+async def test_blocklist_filter_lets_junior_through():
     await process(dict(title="Junior Python Developer"))
 
 
 @pytest.mark.asyncio
-async def test_blocklist_lets_junior_senior_through():
+async def test_blocklist_filter_lets_junior_senior_through():
     await process(dict(title="Python Developer, Junior/Senior"))
 
 
 @pytest.mark.asyncio
-async def test_blocklist_lets_senior_junior_through():
+async def test_blocklist_filter_lets_senior_junior_through():
     await process(dict(title="Python Developer, Senior/Junior"))
 
 
 @pytest.mark.asyncio
-async def test_blocklist_blocks_senior():
+async def test_blocklist_filter_blocks_senior():
     with pytest.raises(DropItem):
         await process(dict(title="Senior Python Developer"))
 
@@ -37,6 +37,6 @@ async def test_blocklist_blocks_senior():
     ],
 )
 @pytest.mark.asyncio
-async def test_blocklist_blocks_machine_programmer(title):
+async def test_blocklist_filter_drops_machine_programmers(title):
     with pytest.raises(DropItem):
         await process(dict(title=title))
