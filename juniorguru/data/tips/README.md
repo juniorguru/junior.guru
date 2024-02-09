3455a0ed46d34098245ac9e3a710bb98b7a6b10b
Honza Javorek <mail@honzajavorek.cz>
2024-02-09 11:33:19 +0100

    rename stuff

--- juniorguru/data/tips/README.md
+++ juniorguru/data/tips/README.md
@@ -1,3 +1,3 @@
 # Tips
 
-Tips for club members. The `jg sync club-tips` script loads them, parses them, and then ensures they appear in the club as individual posts in a special forum channel. The files must fit certain conventions.
+Tips for club members. The `jg sync tips` script loads them, parses them, and then ensures they appear in the club as individual posts in a special forum channel. The files must fit certain conventions.
--- juniorguru/sync/monday_talk.py
+++ juniorguru/sync/monday_talk.py
@@ -15,10 +15,10 @@ from juniorguru.models.tip import Tip
 logger = loggers.from_path(__file__)
 
 
-TALK_EMOJI = "💬"
+MONDAY_TALK_EMOJI = "💬"
 
 
-@cli.sync_command(dependencies=["club-content", "club-tips"])
+@cli.sync_command(dependencies=["club-content", "tips"])
 @click.option("--channel", "channel_id", default="announcements", type=parse_channel)
 @click.option(
     "--today",
@@ -26,34 +26,37 @@ TALK_EMOJI = "💬"
     type=date.fromisoformat,
 )
 def main(channel_id: int, today: date):
-    message = ClubMessage.last_bot_message(channel_id, starting_emoji=TALK_EMOJI)
+    message = ClubMessage.last_bot_message(channel_id, starting_emoji=MONDAY_TALK_EMOJI)
     if message and message.created_at.date() == today:
-        logger.info("Talk announcement already exists")
+        logger.info("Monday talk announcement already exists")
         return
 
     with db.connection_context():
-        tip_url = Tip.get_by_id("talk").club_url
+        tip_url = Tip.get_by_id("monday_talk").club_url
 
     discord_task.run(announce_talk, channel_id, tip_url, today)
 
 
 async def announce_talk(client: ClubClient, channel_id: int, tip_url: str, today: date):
-    talks = filter(is_talk, client.club_guild.scheduled_events)
-    talks = filter(lambda talk: talk.start_time.date() == today, talks)
+    today_monday_talks = (
+        talk
+        for talk in client.club_guild.scheduled_events
+        if talk.start_time.date() == today and is_monday_talk(talk)
+    )
     try:
-        talk = next(talks)
+        monday_talk = next(today_monday_talks)
     except StopIteration:
-        logger.info("No talk today")
+        logger.info("No monday talk today")
         return
 
-    logger.info(f"Announcing talk {talk.url}")
+    logger.info(f"Announcing monday talk {monday_talk.url}")
     text = (
-        f"{TALK_EMOJI} Jako každý týden, i dnes večer bude v klubovně [Pondělní povídání]({talk.url})!\n\n"
+        f"{MONDAY_TALK_EMOJI} Jako každý týden, i dnes večer bude v klubovně [Pondělní povídání]({monday_talk.url})!\n\n"
         f"- Co čekat? Mrkni na {tip_url}"
         "\n"
         f"- Chceš něco konkrétního probrat? Piš do https://discord.com/channels/769966886598737931/1198999483309117582/1198999483309117582"
     )
-    mentions = sorted([user.mention async for user in talk.subscribers()])
+    mentions = sorted([user.mention async for user in monday_talk.subscribers()])
     if mentions:
         text += f"\n\n{' '.join(mentions)} na viděnou, slyšenou večer 👋"
 
@@ -62,6 +65,8 @@ async def announce_talk(client: ClubClient, channel_id: int, tip_url: str, today
         await proxy.send(text, silent=True, suppress=True)
 
 
-def is_talk(scheduled_event: ScheduledEvent) -> bool:
+def is_monday_talk(scheduled_event: ScheduledEvent) -> bool:
+    if scheduled_event.title.lower().strip() != "pondělní povídání":
+        return False
     location_id = getattr(scheduled_event.location.value, "id", None)
     return location_id == ClubChannelID.CLUBHOUSE
