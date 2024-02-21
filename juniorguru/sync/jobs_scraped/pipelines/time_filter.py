ff9fbf18a363059114fd9c8e57d13c9794f919db
Honza Javorek <mail@honzajavorek.cz>
2024-02-21 11:25:41 +0100

    omg, please

--- juniorguru/sync/jobs_scraped/pipelines/time_filter.py
+++ juniorguru/sync/jobs_scraped/pipelines/time_filter.py
@@ -7,5 +7,5 @@ async def process(item: dict, today: date | None = None, days=90) -> dict:
     posted_on = date.fromisoformat(item.get("posted_on") or item["first_seen_on"])
     threshold_on = (today or date.today()) - timedelta(days=days)
     if posted_on < threshold_on:
-        raise DropItem(f"Older than {days} days: {item['posted_on']}")
+        raise DropItem(f"Older than {days} days: {posted_on}")
     return item
