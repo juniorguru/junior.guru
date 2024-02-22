c57cd0190a4ac6a2da44185bff6a0d6501786a98
Honza Javorek <mail@honzajavorek.cz>
2024-02-22 14:28:21 +0100

    test stages

---
+++ tests/test_models_stage.py
@@ -0,0 +1,32 @@
+import random
+from typing import Generator
+import uuid
+import pytest
+from juniorguru.models.base import SqliteDatabase
+from juniorguru.models.stage import Stage
+
+from testing_utils import prepare_test_db
+
+
+@pytest.fixture
+def test_db() -> Generator[SqliteDatabase, None, None]:
+    yield from prepare_test_db([Stage])
+
+
+def create_stage(**kwargs) -> Stage:
+    unique_id = uuid.uuid4().hex
+    return Stage.create(
+        position=kwargs.get("position", random.randint(1, 100)),
+        slug=kwargs.get("slug", f"learning-{unique_id}"),
+        title=kwargs.get("title", f"Učím se #{unique_id}"),
+        icon=kwargs.get("icon", f"scooter-{unique_id}"),
+        description=kwargs.get("description", "Bla bla bla."),
+    )
+
+
+def test_listing_sorts_by_position(test_db: SqliteDatabase):
+    stage3 = create_stage(position=3)
+    stage1 = create_stage(position=1)
+    stage2 = create_stage(position=2)
+
+    assert list(Stage.listing()) == [stage1, stage2, stage3]
