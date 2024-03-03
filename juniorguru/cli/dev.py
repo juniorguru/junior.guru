e7ecc6ba0498e605899832032801decae331e38e
Honza Javorek <mail@honzajavorek.cz>
2024-03-03 22:39:30 +0100

    use ruff

--- juniorguru/cli/dev.py
+++ juniorguru/cli/dev.py
@@ -62,7 +62,7 @@ def update(pull, packages, push, stash):
 @main.command()
 def lint():
     try:
-        subprocess.run(["flake8"], check=True)
+        subprocess.run(["ruff", "check"], check=True)
         subprocess.run(
             [
                 "npx",
