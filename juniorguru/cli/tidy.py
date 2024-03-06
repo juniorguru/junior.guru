34a352c38ef2a8f4544a50952a68093f0852f39d
Honza Javorek <mail@honzajavorek.cz>
2024-03-06 12:59:03 +0100

    tidy jinja imports

--- juniorguru/cli/tidy.py
+++ juniorguru/cli/tidy.py
@@ -1,13 +1,43 @@
 import itertools
 import math
+import re
 import subprocess
 from io import BytesIO
 from pathlib import Path
+from typing import Iterable
+from arrow import get
 
 import click
 from PIL import Image
 
 from juniorguru.lib import loggers
+
+
+JINJA_IMPORT_RE = re.compile(
+    r"""
+        {%\s*
+            from\s+
+            (?P<source>'[^']+'|"[^"]+")\s+
+            import
+            (?P<names>[\w\s_,]+)
+        \s*%}
+    """,
+    re.VERBOSE,
+)
+
+JINJA_CALL_RE = re.compile(
+    r"""
+        {{\s*
+            ([\w_]+)
+            \(
+        |
+        {%\s*
+            call\s*
+            ([\w_]+)
+            \(
+    """,
+    re.VERBOSE,
+)
 
 
 logger = loggers.from_path(__file__)
@@ -30,6 +60,22 @@ def format_python():
         subprocess.run(["ruff", "format"], check=True)
     except subprocess.CalledProcessError:
         raise click.Abort()
+
+
+@main.command()
+def unused_macros():
+    cwd = Path.cwd()
+    web_dir = Path("juniorguru/web/").resolve()
+    paths = itertools.chain(web_dir.rglob("*.jinja"), web_dir.rglob("*.md"))
+    for path in paths:
+        logger.info(path.relative_to(cwd))
+        markup = path.read_text()
+        imports = get_jinja_imports(markup)
+        calls = get_jinja_calls(markup)
+        unused_imports = imports - calls
+        if unused_imports:
+            logger.warning(f"Unused imports: {', '.join(unused_imports)}")
+            path.write_text(replace_jinja_imports(markup, calls))
 
 
 @main.command()
@@ -79,5 +125,50 @@ def optimize_svg():
             )
 
 
-def kilobytes(size):
+def kilobytes(size) -> int:
     return math.ceil(size / 1000)
+
+
+def get_jinja_imports(markup: str) -> set[str]:
+    all_names = set()
+    matches = list(JINJA_IMPORT_RE.finditer(markup))
+    if matches:
+        if len(matches) > 1:
+            raise ValueError("Multiple Jinja imports found")
+        names = (
+            name.strip()
+            for name in (
+                re.sub(r"\s+", " ", matches[0].group("names"))
+                .strip()
+                .removesuffix("with context")
+                .removesuffix("without context")
+                .split(",")
+            )
+        )
+        all_names.update(names)
+    return all_names
+
+
+def get_jinja_calls(markup: str) -> set[str]:
+    all_names = set()
+    for match in JINJA_CALL_RE.finditer(markup):
+        names = filter(None, match.groups())
+        all_names.update(names)
+    return all_names
+
+
+def replace_jinja_imports(markup: str, names: Iterable[str]) -> str:
+    matches = list(JINJA_IMPORT_RE.finditer(markup))
+    if matches:
+        if len(matches) > 1:
+            raise ValueError("Multiple Jinja imports found")
+        match = matches[0]
+        old_names = match.group("names")
+        new_names = ", ".join(sorted(names))
+        if old_names.endswith("with context"):
+            new_names += " with context"
+        if old_names.endswith("without context"):
+            new_names += " without context"
+        new_import = f"{{% from {match.group('source')} import {new_names} %}}"
+        return JINJA_IMPORT_RE.sub(new_import, markup)
+    return markup
---
+++ tests/test_cli_tidy.py
@@ -0,0 +1,128 @@
+from textwrap import dedent
+
+import pytest
+
+from juniorguru.cli.tidy import get_jinja_calls, get_jinja_imports
+
+
+@pytest.mark.parametrize(
+    "markup, expected",
+    [
+        (
+            dedent(
+                """
+                    {% from 'macros.html' import blockquote, blockquote_avatar,
+                        blockquote_toxic, lead
+                    with context %}
+                """
+            ),
+            {"blockquote", "blockquote_avatar", "blockquote_toxic", "lead"},
+        ),
+        (
+            dedent(
+                """
+                    {% from 'macros.html' import blockquote, blockquote_avatar without context %}
+                """
+            ),
+            {"blockquote", "blockquote_avatar"},
+        ),
+        (
+            dedent(
+                """
+                    {% from 'macros.html' import blockquote, blockquote_avatar %}
+                """
+            ),
+            {"blockquote", "blockquote_avatar"},
+        ),
+    ],
+)
+def test_get_jinja_imports(markup: str, expected: set[str]):
+    assert get_jinja_imports(markup) == expected
+
+
+@pytest.mark.parametrize(
+    "markup",
+    [
+        dedent(
+            """
+                {% from 'macros.html' import blockquote, blockquote_avatar %}
+                foo boo
+                moo
+                {% from 'macros.html' import lead, note %}
+            """
+        ),
+        dedent(
+            """
+                {% from 'macros.html' import blockquote, blockquote_avatar %}
+                foo boo
+                moo
+                {% from 'macros.html' import blockquote, lead, note %}
+            """
+        ),
+    ],
+)
+def test_get_jinja_imports_raises(markup: str):
+    with pytest.raises(ValueError):
+        assert get_jinja_imports(markup)
+
+
+@pytest.mark.parametrize(
+    "markup, expected",
+    [
+        (
+            dedent(
+                """
+                    {{ video_card_engeto(
+                        'Nejčastější mýty o práci v IT',
+                        '5min',
+                        'https://www.youtube.com/watch?v=2Km3orTYFrM&list=PLrsbT5TVJXZa2daxo8_3NagDzPqHjBEpI',
+                        'Musím mít talent na techniku nebo matematiku? Záleží na věku? Potřebuji vysokou školu?',
+                    ) }}
+                """
+            ),
+            {"video_card_engeto"},
+        ),
+        (
+            dedent(
+                """
+                    {{ link_card(
+                        'Make',
+                        'https://www.make.com/',
+                        'Největší platforma, podporuje i české služby jako Fakturoid nebo Fio Banka.'
+                    ) }}
+
+                    {{ link_card(
+                        'Zapier',
+                        'https://zapier.com/',
+                        'Druhá nejznámější platforma pro automatizaci.'
+                    ) }}
+                """
+            ),
+            {"link_card"},
+        ),
+        (
+            dedent(
+                """
+                    {% call note(standout=True) %}
+                        {{ 'exclamation-circle'|icon }} Kapitola se teprve připravuje.
+                    {% endcall %}
+                """
+            ),
+            {"note"},
+        ),
+        (
+            dedent(
+                """
+                    {{ img('static/chick' + loop.index|string + '.svg', 'Kuře', 50, 50, lazy=False) }}
+
+                    {% call note(standout=True) %}
+                        {{ 'exclamation-circle'|icon }} Kapitola se teprve připravuje.
+                    {% endcall %}
+                """
+            ),
+            {"img", "note"},
+        ),
+    ],
+)
+def test_get_jinja_calls(markup: str, expected: set[str]):
+    assert get_jinja_calls(markup) == expected
