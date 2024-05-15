3f5d81f1386c23c027533ea429de263389178504
Honza Javorek <mail@honzajavorek.cz>
2024-05-15 13:49:53 +0200

    read profiles from api

--- jg/coop/models/candidate.py
+++ jg/coop/models/candidate.py
@@ -1,13 +1,22 @@
 from typing import Iterable, Self
 
-from peewee import CharField
+from peewee import CharField, ForeignKeyField
 
 from jg.coop.models.base import BaseModel
+from jg.coop.models.club import ClubUser
 
 
 class Candidate(BaseModel):
-    name = CharField()
+    github_username = CharField()
+    user = ForeignKeyField(ClubUser, null=True, unique=True)
 
     @classmethod
     def listing(cls) -> Iterable[Self]:
         return cls.select()
+
+    @classmethod
+    def from_api(cls, profile: dict) -> Self:
+        return cls(
+            github_username=profile["username"],
+            user=profile.get("discord_id"),
+        )
--- jg/coop/sync/candidates.py
+++ jg/coop/sync/candidates.py
@@ -1,3 +1,6 @@
+import click
+import requests
+
 from jg.coop.cli.sync import main as cli
 from jg.coop.lib import loggers
 from jg.coop.models.base import db
@@ -7,8 +10,17 @@ from jg.coop.models.candidate import Candidate
 logger = loggers.from_path(__file__)
 
 
-@cli.sync_command(dependencies=["club-content", "roles"])
+@cli.sync_command(dependencies=["club-content"])
+@click.option("--api-url", default="https://juniorguru.github.io/eggtray/profiles.json")
 @db.connection_context()
-def main():
+def main(api_url: str):
     Candidate.drop_table()
     Candidate.create_table()
+
+    logger.info("Reading API")
+    response = requests.get(api_url)
+    response.raise_for_status()
+    for profile in response.json():
+        candidate = Candidate.from_api(profile)
+        candidate.save()
+        logger.info(f"Saved {candidate.github_username!r} ( ↔ {candidate.user!r})")
