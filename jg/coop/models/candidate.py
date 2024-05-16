5bb0badc758d1774bc6ae3a0874cfb9f0536a826
Honza Javorek <mail@honzajavorek.cz>
2024-05-16 21:54:55 +0200

    candidates models

--- jg/coop/models/candidate.py
+++ jg/coop/models/candidate.py
@@ -1,22 +1,52 @@
 from typing import Iterable, Self
 
-from peewee import CharField, ForeignKeyField
+from peewee import (
+    BooleanField,
+    CharField,
+    Check,
+    DateField,
+    ForeignKeyField,
+    IntegerField,
+)
 
-from jg.coop.models.base import BaseModel
+from jg.coop.models.base import BaseModel, JSONField
 from jg.coop.models.club import ClubUser
 
 
 class Candidate(BaseModel):
-    github_username = CharField()
+    github_username = CharField(primary_key=True)
+    github_url = CharField()
     user = ForeignKeyField(ClubUser, null=True, unique=True)
+    name = CharField()
+    bio = CharField(null=True)
+    email = CharField(null=True)
+    avatar_url = CharField()
+    location = CharField(null=True)
+    linkedin_url = CharField(null=True)
+    topics = JSONField(default=list)
+    domains = JSONField(default=list)
+    experience = JSONField(default=list)
+    secondary_school = CharField(null=True)
+    university = CharField(null=True)
+    languages = JSONField(default=list)
+    is_ready = BooleanField()
+    is_member = BooleanField()
 
     @classmethod
     def listing(cls) -> Iterable[Self]:
-        return cls.select()
-
-    @classmethod
-    def from_api(cls, profile: dict) -> Self:
-        return cls(
-            github_username=profile["username"],
-            user=profile.get("discord_id"),
+        return cls.select().order_by(
+            cls.is_ready.desc(), cls.is_member.desc(), cls.name
         )
+
+
+class CandidateProject(BaseModel):
+    name = CharField(primary_key=True)
+    candidate = ForeignKeyField(Candidate, backref="list_projects")
+    title = CharField(null=True)
+    source_url = CharField()
+    live_url = CharField(null=True)
+    description = CharField(null=True)
+    priority = IntegerField(constraints=[Check("priority == 0 or priority == 1")])
+    start_on = DateField()
+    end_on = DateField()
+    topics = JSONField(default=list)
--- jg/coop/sync/candidates.py
+++ jg/coop/sync/candidates.py
@@ -4,7 +4,7 @@ import requests
 from jg.coop.cli.sync import main as cli
 from jg.coop.lib import loggers
 from jg.coop.models.base import db
-from jg.coop.models.candidate import Candidate
+from jg.coop.models.candidate import Candidate, CandidateProject
 
 
 logger = loggers.from_path(__file__)
@@ -14,13 +14,23 @@ logger = loggers.from_path(__file__)
 @click.option("--api-url", default="https://juniorguru.github.io/eggtray/profiles.json")
 @db.connection_context()
 def main(api_url: str):
-    Candidate.drop_table()
-    Candidate.create_table()
+    db.drop_tables([Candidate, CandidateProject])
+    db.create_tables([Candidate, CandidateProject])
 
     logger.info("Reading API")
     response = requests.get(api_url)
     response.raise_for_status()
-    for profile in response.json()["items"]:
-        candidate = Candidate.from_api(profile)
-        candidate.save()
-        logger.info(f"Saved {candidate.github_username!r} ( ↔ {candidate.user!r})")
+
+    for candidate_item in response.json()["items"]:
+        discord_id = candidate_item.pop("discord_id", None)
+        projects_items = candidate_item.pop("projects", [])
+
+        candidate = Candidate.create(user=discord_id, is_member=False, **candidate_item)
+        if candidate.user:
+            candidate.is_member = candidate.user.is_member
+            candidate.save()
+        logger.info(f"Saved {candidate!r} ( ↔ {candidate.user!r})")
+
+        for project_item in projects_items:
+            CandidateProject.create(candidate=candidate, **project_item)
+        logger.info(f"Saved {len(projects_items)} projects for {candidate!r}")
