8a079770783de50dda3e6da0cb0e96b73744f526
Honza Javorek <mail@honzajavorek.cz>
2024-05-31 18:02:34 +0200

    try replicate, change prompt, etc.

--- jg/coop/sync/jobs_scraped/pipelines/juniority_filter.py
+++
@@ -1,9 +0,0 @@
-from jg.coop.sync.jobs_scraped import DropItem
-
-
-async def process(item: dict) -> dict:
-    if llm_opinion := item.get("llm_opinion"):
-        if llm_opinion["is_entry_level"]:
-            return item
-        raise DropItem(f"Not for juniors: {llm_opinion['reason_cs']!r}")
-    raise DropItem("Missing LLM opinion")
