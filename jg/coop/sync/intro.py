878683bdea537e35a8b55867dbbac03fef6fec93
Honza Javorek <mail@honzajavorek.cz>
2024-06-11 10:33:42 +0200

    debugging

--- jg/coop/sync/intro.py
+++ jg/coop/sync/intro.py
@@ -69,14 +69,26 @@ async def process_message(discord_channel: TextChannel, message: ClubMessage):
         logger.debug(f"Message {message.url} sent by the bot itself, skipping")
         return
     if message.type == "default" and message.is_intro:
-        logger.info(f"Welcoming member #{message.author.id}")
-        await welcome(discord_channel, message)
+        try:
+            logger.info(f"Welcoming member #{message.author.id}")
+            await welcome(discord_channel, message)
+        except discord.NotFound:
+            logger.error(
+                f"WELCOME: Message {message.url} by {message.author.name}, {message.created_at} not found in the channel"
+            )
+            raise
     elif (
         message.type == "new_member"
         and message.author.first_seen_on() < message.created_at.date()
     ):
-        logger.info(f"Welcoming back member #{message.author.id}")
-        await welcome_back(discord_channel, message)
+        try:
+            logger.info(f"Welcoming back member #{message.author.id}")
+            await welcome_back(discord_channel, message)
+        except discord.NotFound:
+            logger.error(
+                f"WELCOME BACK: Message {message.url} by {message.author.name}, {message.created_at} not found in the channel"
+            )
+            raise
 
 
 @mutations.mutates_discord()
