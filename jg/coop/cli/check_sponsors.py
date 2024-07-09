979075f078530fd8f3692a2e0c3a5e621f391a51
Honza Javorek <mail@honzajavorek.cz>
2024-07-09 16:29:10 +0200

    omg kill me now

--- jg/coop/cli/check_sponsors.py
+++ jg/coop/cli/check_sponsors.py
@@ -6,18 +6,17 @@ import yaml
 
 from jg.coop.lib import loggers
 from jg.coop.models.base import db
-from jg.coop.sync.organizations import SPONSORS_YAML_PATH, SponsorsConfig, get_renews_on
 
 
 logger = loggers.from_path(__file__)
 
 
 @click.command()
-# @click.option(
-#     "--path",
-#     default=SPONSORS_YAML_PATH,
-#     type=click.Path(path_type=Path),
-# )
+@click.option(
+    "--path",
+    default=Path("jg/coop/data/sponsors.yml"),
+    type=click.Path(path_type=Path),
+)
 @click.option(
     "--today",
     default=lambda: date.today().isoformat(),
@@ -26,27 +25,28 @@ logger = loggers.from_path(__file__)
 @click.option("--weekday", default=1, type=int)
 @click.option("--days", default=60, type=int)
 @db.connection_context()
-def main(today: date, weekday: int, days: int):  # path: Path
-    pass
-    # if today.isoweekday() != weekday:
-    #     logger.warning(f"No check today (weekday {today.isoweekday()} ≠ {weekday})")
-    #     return
+def main(path: Path, today: date, weekday: int, days: int):
+    from jg.coop.sync.organizations import SponsorsConfig, get_renews_on  # TODO FIXME
 
-    # logger.info(f"Loading sponsors data from {path}")
-    # sponsors_yaml_data = yaml.safe_load(path.read_text())
-    # sponsors = SponsorsConfig(**sponsors_yaml_data)
+    if today.isoweekday() != weekday:
+        logger.warning(f"No check today (weekday {today.isoweekday()} ≠ {weekday})")
+        return
 
-    # renewals = []
-    # for sponsor in sponsors.registry:
-    #     if renews_on := get_renews_on(sponsor.periods, today):
-    #         logger.info(f"{sponsor.name} renews on {renews_on}")
+    logger.info(f"Loading sponsors data from {path}")
+    sponsors_yaml_data = yaml.safe_load(path.read_text())
+    sponsors = SponsorsConfig(**sponsors_yaml_data)
 
-    #         remains_days = (renews_on - today).days
-    #         if remains_days <= days:
-    #             logger.error(f"{sponsor.name} renews soon! ({remains_days} days)")
-    #             renewals.append(sponsor)
-    #     else:
-    #         logger.debug(f"{sponsor.name} does not renew")
+    renewals = []
+    for sponsor in sponsors.registry:
+        if renews_on := get_renews_on(sponsor.periods, today):
+            logger.info(f"{sponsor.name} renews on {renews_on}")
 
-    # if renewals:
-    #     raise click.Abort()
+            remains_days = (renews_on - today).days
+            if remains_days <= days:
+                logger.error(f"{sponsor.name} renews soon! ({remains_days} days)")
+                renewals.append(sponsor)
+        else:
+            logger.debug(f"{sponsor.name} does not renew")
+
+    if renewals:
+        raise click.Abort()
