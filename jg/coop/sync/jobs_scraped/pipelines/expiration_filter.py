4b93b2df236e7525662b797eee54644b58ed422d
Honza Javorek <mail@honzajavorek.cz>
2024-10-21 12:25:09 +0200

    omg git

---
+++ jg/coop/sync/jobs_scraped/pipelines/expiration_filter.py
@@ -0,0 +1,9 @@
+from jg.coop.sync.jobs_scraped import DropItem
+
+
+async def process(item: dict) -> dict:
+    # try: TODO TODO TODO
+    #     url = item["url"]
+    # except KeyError as e:
+    #     raise DropItem(str(e))
+    return item
