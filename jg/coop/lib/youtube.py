153b1e78471984ecb791c4ae3f32d17c7d00f5eb
Honza Javorek <mail@honzajavorek.cz>
2025-05-20 13:53:30 +0200

    add support for fetching and parsing youtube info

--- jg/coop/lib/youtube.py
+++ jg/coop/lib/youtube.py
@@ -1,9 +1,33 @@
 import re
+from datetime import datetime
+from typing import Annotated
+
+from openai import BaseModel
+from pydantic import HttpUrl, PlainSerializer
+from yt_dlp import YoutubeDL
 
 
 YOUTUBE_URL_RE = re.compile(
     r"(youtube\.com.+watch\?.*v=|youtube\.com/live/|youtu\.be/)([\w\-\_]+)"
 )
+
+
+class YouTubeInfo(BaseModel):
+    id: str
+    url: Annotated[HttpUrl, PlainSerializer(str)]
+    title: str
+    title_full: str
+    thumbnail_url: Annotated[HttpUrl, PlainSerializer(str)]
+    description: str
+    duration: str
+    duration_s: int
+    view_count: int
+    comment_count: int
+    like_count: int
+    tags: list[str]
+    released_at: datetime
+    uploaded_at: datetime
+    chapters: list[str]
 
 
 def parse_youtube_id(url) -> str:
@@ -12,3 +36,28 @@ def parse_youtube_id(url) -> str:
         return match.group(2)
     except AttributeError:
         raise ValueError(f"URL {url} doesn't contain YouTube ID")
+
+
+def parse_youtube_info(info: dict) -> YouTubeInfo:
+    return YouTubeInfo(
+        id=info["id"],
+        url=info["webpage_url"],
+        title=info["title"],
+        title_full=info["fulltitle"],
+        thumbnail_url=info["thumbnail"],
+        description=info["description"],
+        duration=info["duration_string"],
+        duration_s=info["duration"],
+        view_count=info["view_count"],
+        comment_count=info.get("comment_count", 0),
+        like_count=info.get("like_count", 0),
+        tags=info.get("tags", []),
+        released_at=datetime.fromtimestamp(info["release_timestamp"]),
+        uploaded_at=datetime.fromtimestamp(info["timestamp"]),
+        chapters=[chapter["title"] for chapter in info.get("chapters", [])],
+    )
+
+
+def fetch_youtube_info(url: str) -> YouTubeInfo:
+    with YoutubeDL({"quiet": True, "no_warnings": True}) as yt:
+        return yt.extract_info(url, download=False)
---
+++ tests/lib_youtube/test_youtube.py
@@ -0,0 +1,96 @@
+import json
+from datetime import datetime
+from pathlib import Path
+
+import pytest
+
+from jg.coop.lib.youtube import parse_youtube_id, parse_youtube_info
+
+
+@pytest.mark.parametrize(
+    "url,expected",
+    [
+        ("https://www.youtube.com/watch?v=0v5K4GvK4Gs", "0v5K4GvK4Gs"),
+        ("https://youtu.be/0v5K4GvK4Gs", "0v5K4GvK4Gs"),
+        (
+            "https://www.youtube.com/watch?v=3-wsqhCK-wU&list=PLhB6F20C-jTPITEXEHus6fVZDfNxzRbv_&index=5",
+            "3-wsqhCK-wU",
+        ),
+    ],
+)
+def test_parse_youtube_id(url, expected):
+    assert parse_youtube_id(url) == expected
+
+
+def test_parse_youtube_id_raises():
+    with pytest.raises(ValueError):
+        parse_youtube_id("https://junior.guru")
+
+
+def test_parse_youtube_info():
+    raw_info = json.loads((Path(__file__).parent / "info.json").read_text())
+    info = parse_youtube_info(raw_info)
+
+    assert info.model_dump() == {
+        "id": "8_ZUwRKEJ7A",
+        "url": "https://www.youtube.com/watch?v=8_ZUwRKEJ7A",
+        "title": "Daniel Srb: Jak na CV při změně kariéry do IT (přednáška v rámci Týdne pro Digitální Česko)",
+        "title_full": "Daniel Srb: Jak na CV při změně kariéry do IT (přednáška v rámci Týdne pro Digitální Česko)",
+        "thumbnail_url": "https://i.ytimg.com/vi/8_ZUwRKEJ7A/maxresdefault.jpg",
+        "description": "Jak napsat životopis při změně kariéry do IT? Záznam akce pro širokou veřejnost v rámci Týdne pro Digitální Česko. Všechny akce pod hlavičkou junior.guru najdeš na https://junior.guru/events/\n\n~~~ Popis ~~~\nDaniel Srb, který viděl spoustu CV, když nabíral vývojáře, pracoval také jako designér a již několik let provází klienty změnou kariéry do IT, ukáže, jak vytvořit efektivní životopis pro hledání první práce v IT. Představí i šablonu, která tě zdarma provede tvorbou kvalitního CV.\n\n~~~ Kapitoly ~~~\n0:00:00 Začátek streamu\n0:01:05 Úvod JG: Honza Javorek\n0:07:04 Začátek přednášky\n0:09:05 Proč CV věnovat pozornost\n0:12:26 Co o CV vlastně vím?\n0:16:08 Kvalitní CV\n0:17:59 Funkčnost CV\n0:22:36 Otázky\n0:31:09 Obsah CV\n0:33:37 Co do CV nepatří\n0:40:13 Otázky\n0:43:23 Co do CV patří\n0:45:37 Jméno a příjmení\n0:46:33 Název pozice\n0:47:11 Kontakty\n0:52:20 Souhrn\n1:06:04 Dovednosti\n1:13:50 Projekty\n1:31:24 Pracovní zkušenosti\n1:45:04 Vzdělání\n1:56:56 Soft skills\n2:01:10 Jazyky\n2:05:31 Zájmy\n2:08:13 Otázky\n2:15:35 Proces tvorby obsahu\n2:24:35 Nástroje na tvorbu CV\n2:31:22 Šablona CV pro switchery do IT\n2:35:12 Vzhled\n2:48:34 Dobře čitelný text\n2:53:12 Písmo\n3:05:42 Velikosti písma\n3:08:10 Vyznačování\n3:10:44 Logické celky a nadpisy\n3:12:45 Vynucené zalomení řádku\n3:13:51 Odkazy\n3:17:36 Odrážky\n3:19:02 Zarovnání textu\n3:21:25 Kdy porušit pravidla\n3:22:21 Délka CV (a jak zkrátit)\n3:31:35 Barvy\n3:33:51 Otázky\n3:35:57 Message from our sponsor\n3:37:14 Ukončení přednášky\n3:38:31 Ukončení přenosu: Honza Javorek\n3:43:28 Pozvánka na další stream JG",
+        "duration": "3:46:35",
+        "duration_s": 13595,
+        "view_count": 3346,
+        "comment_count": 2,
+        "like_count": 24,
+        "tags": [],
+        "released_at": datetime(2024, 11, 19, 18, 10, 3),
+        "uploaded_at": datetime(2024, 11, 20, 10, 18, 13),
+        "chapters": [
+            "Začátek streamu",
+            "Úvod JG: Honza Javorek",
+            "Začátek přednášky",
+            "Proč CV věnovat pozornost",
+            "Co o CV vlastně vím?",
+            "Kvalitní CV",
+            "Funkčnost CV",
+            "Otázky",
+            "Obsah CV",
+            "Co do CV nepatří",
+            "Otázky",
+            "Co do CV patří",
+            "Jméno a příjmení",
+            "Název pozice",
+            "Kontakty",
+            "Souhrn",
+            "Dovednosti",
+            "Projekty",
+            "Pracovní zkušenosti",
+            "Vzdělání",
+            "Soft skills",
+            "Jazyky",
+            "Zájmy",
+            "Otázky",
+            "Proces tvorby obsahu",
+            "Nástroje na tvorbu CV",
+            "Šablona CV pro switchery do IT",
+            "Vzhled",
+            "Dobře čitelný text",
+            "Písmo",
+            "Velikosti písma",
+            "Vyznačování",
+            "Logické celky a nadpisy",
+            "Vynucené zalomení řádku",
+            "Odkazy",
+            "Odrážky",
+            "Zarovnání textu",
+            "Kdy porušit pravidla",
+            "Délka CV (a jak zkrátit)",
+            "Barvy",
+            "Otázky",
+            "Message from our sponsor",
+            "Ukončení přednášky",
+            "Ukončení přenosu: Honza Javorek",
+            "Pozvánka na další stream JG",
+        ],
+    }
