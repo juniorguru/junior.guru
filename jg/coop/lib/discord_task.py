12efc5b5b83013ae0fdf133b8565773fcf723ab6
Honza Javorek <mail@honzajavorek.cz>
2025-06-09 13:14:00 +0200

    smarter and better exc handling, remove hotfix leftover

--- jg/coop/lib/discord_task.py
+++ jg/coop/lib/discord_task.py
@@ -1,6 +1,8 @@
 import asyncio
 import os
+import sys
 import threading
+from queue import Queue
 from typing import Awaitable, Callable
 
 from jg.coop.lib import loggers
@@ -20,59 +22,44 @@ def run(task_fn: Callable[..., Awaitable], *args, **kwargs) -> None:
     Separate process is used so that it's possible to run multiple one-time
     async tasks independently on each other, in separate async loops.
     """
-    exc = None
+    exc_queue = Queue()
 
     def _discord_thread(task_fn: Callable[..., Awaitable], args, kwargs) -> None:
         if not asyncio.iscoroutinefunction(task_fn):
-            raise TypeError(
+            exc = TypeError(
                 f"Not async function: {task_fn.__qualname__} from {task_fn.__module__}"
             )
+            exc_queue.put(exc)
 
         class Client(ClubClient):
             async def on_ready(self):
                 await self.wait_until_ready()
                 logger.debug("Discord connection ready")
-                await task_fn(self, *args, **kwargs)
-                logger.debug("Closing Discord client")
-                await self.close()
+                try:
+                    await task_fn(self, *args, **kwargs)
+                except Exception as exc:
+                    logger.debug("Task function raised an exception")
+                    exc_queue.put(exc)
+                finally:
+                    logger.debug("Closing Discord client")
+                    await self.close()
 
             async def on_error(self, event, *args, **kwargs):
-                logger.debug("Got an error, raising")
-                raise
+                logger.debug("Got an error")
+                exc_type, exc_value, tb = sys.exc_info()
+                exc_queue.put(exc_value)
 
-            async def close(self):
-                # See https://github.com/Pycord-Development/pycord/pull/2618
-                if self._closed:
-                    return
-                await self.http.close()
-                self._closed = True
-                for voice in self.voice_clients:
-                    try:
-                        await voice.disconnect(force=True)
-                    except Exception:
-                        pass
-                if self.ws is not None and self.ws.open:
-                    await self.ws.close(code=1000)
-                self._ready.clear()
-
-        client = Client(loop=asyncio.new_event_loop())
-
-        def exc_handler(loop, context):
-            nonlocal exc
-            exc = context.get("exception")
-            logger.debug(f"Recording exception: {exc}")
-            loop.default_exception_handler(context)
-            logger.debug("Stopping async execution")
-            loop.stop()
-
-        client.loop.set_exception_handler(exc_handler)
         logger.debug("Starting")
-        client.run(DISCORD_API_KEY)
+        try:
+            client = Client(loop=asyncio.new_event_loop())
+            client.run(DISCORD_API_KEY)
+        except Exception as exc:
+            logger.debug("Discord client raised an exception")
+            exc_queue.put(exc)
 
     thread = threading.Thread(target=_discord_thread, args=[task_fn, args, kwargs])
     thread.start()
     thread.join()
 
-    if exc:
-        logger.debug("Found exception, raising")
-        raise exc
+    if not exc_queue.empty():
+        raise exc_queue.get()
