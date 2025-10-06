32222081c77c167c77f151116a102a164c647a3a
Honza Javorek <mail@honzajavorek.cz>
2025-10-06 09:49:17 +0200

    sync subscribers - unfinished stub

--- src/jg/coop/lib/buttondown.py
+++ src/jg/coop/lib/buttondown.py
@@ -1,5 +1,6 @@
 import os
 from datetime import date
+from enum import StrEnum
 from typing import Self
 
 import httpx
@@ -13,8 +14,28 @@ BUTTONDOWN_API_KEY = os.getenv("BUTTONDOWN_API_KEY")
 logger = loggers.from_path(__file__)
 
 
+class SubscriberSource(StrEnum):
+    ECOMAIL = "ecomail"
+    MAILCHIMP = "mailchimp"
+    MEMBERFUL = "memberful"
+
+
+class SubscriberErrorCode(StrEnum):
+    EMAIL_ALREADY_EXISTS = "email_already_exists"
+    EMAIL_BLOCKED = "email_blocked"
+    EMAIL_INVALID = "email_invalid"
+    IP_ADDRESS_SPAMMY = "ip_address_spammy"
+    METADATA_INVALID = "metadata_invalid"
+    RATE_LIMITED = "rate_limited"
+    SUBSCRIBER_ALREADY_EXISTS = "subscriber_already_exists"
+    SUBSCRIBER_BLOCKED = "subscriber_blocked"
+    SUBSCRIBER_SUPPRESSED = "subscriber_suppressed"
+    TAG_INVALID = "tag_invalid"
+
+
 class ButtondownAPI:
-    def __init__(self, token: str | None = None):
+
+    def __init__(self, token: str | None = None) -> None:
         self.token = token or BUTTONDOWN_API_KEY
         self._client = None
 
@@ -26,7 +47,7 @@ class ButtondownAPI:
         )
         return self
 
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
+    async def __aexit__(self, exc_type, exc_val, exc_tb) -> None:
         if self._client:
             await self._client.aclose()
 
@@ -42,3 +63,43 @@ class ButtondownAPI:
         response = await self._client.post("emails", json=email_data)
         response.raise_for_status()
         return response.json()
+
+    # @mutations.mutates_buttondown()
+    async def add_subscriber(self, email: str, tags=set[SubscriberSource]) -> None:
+        # first get subscriber by email, check if all is ok
+        # if not, either update tags or create
+        try:
+            response = await self._client.get(f"subscribers/{email}")
+            response.raise_for_status()
+            subscriber = response.json()
+            existing_tags = set(subscriber.get("tags", []))
+            print(existing_tags)
+        except httpx.HTTPStatusError as e:
+            if e.response.status_code == 404:
+                response = await self._client.post(
+                    "subscribers",
+                    json={
+                        "email_address": email,
+                        "tags": list(tags),
+                        "type": "regular",
+                    },
+                )
+                response.raise_for_status()
+            else:
+                raise
+
+        # response = await self._client.post(
+        #     "subscribers",
+        #     json={"email_address": email, "tags": list(tags), "type": "regular"},
+        # )
+        # try:
+        #     response.raise_for_status()
+        # except httpx.HTTPStatusError as e:
+        #     if e.response.status_code != 400:
+        #         raise
+        #     error_data = e.response.json()
+        #     error_code = error_data.get("code")
+        #     if error_code == SubscriberErrorCode.EMAIL_ALREADY_EXISTS:
+        #         subscriber_id = error_data["metadata"]["subscriber_id"]
+        #         raise EmailAreadyExistsError(subscriber_id) from e
+        # return response.json()
---
+++ src/jg/coop/sync/newsletter_subscribers/__init__.py
@@ -0,0 +1,94 @@
+from pathlib import Path
+
+import click
+import httpx
+
+from jg.coop.cli.sync import default_from_env, main as cli
+from jg.coop.lib import loggers
+from jg.coop.lib.buttondown import ButtondownAPI, SubscriberErrorCode
+from jg.coop.lib.cache import get_cache
+from jg.coop.lib.cli import async_command
+from jg.coop.lib.memberful import MemberfulAPI
+from jg.coop.models.base import db
+
+
+MEMBERS_GQL_PATH = Path(__file__).parent / "members.gql"
+
+
+logger = loggers.from_path(__file__)
+
+
+@cli.sync_command()
+@click.option("--cache-key", default="newsletter:subscribers")
+@click.option("--cache-hrs", default=60, type=int)
+@click.option("--ecomail-api-key", default=default_from_env("ECOMAIL_API_KEY"))
+@click.option("--ecomail-list", "ecomail_list_id", default=1, type=int)
+@db.connection_context()
+@async_command
+async def main(cache_key: str, cache_hrs: int, ecomail_api_key: str, ecomail_list_id: int):
+    cache = get_cache()
+    if subscribers := cache.get(cache_key):
+        logger.info(f"Using cached {len(subscribers)} subscribers")
+    else:
+        subscribers = {}
+
+        logger.info("Fetching subscribers from Memberful")
+        memberful = MemberfulAPI()
+        for member in memberful.get_nodes(MEMBERS_GQL_PATH.read_text()):
+            subscribers.setdefault(member["email"], set())
+            subscribers[member["email"]].add("memberful")
+        logger.info(f"Fetched {len(subscribers)} subscribers from Memberful")
+
+        logger.info("Fetching subscribers from Ecomail")
+        # According to https://ecomailczv2.docs.apiary.io/
+        # Status = 1: subscribed, 2: unsubscribed, 3: soft bounce, 4: hard bounce, 5: spam complaint, 6: unconfirmed
+        ecomail_page_url = f"https://api2.ecomailapp.cz/lists/{ecomail_list_id}/subscribers"
+        ecomail_headers = {
+            "key": ecomail_api_key,
+            "User-Agent": "JuniorGuruBot (+https://junior.guru)",
+        }
+        ecomail_count = 0
+        async with httpx.AsyncClient(headers=ecomail_headers) as client:
+            for page in range(1, 100):  # safety break at 100 pages
+                logger.debug(f"Getting subscribers from {ecomail_page_url}")
+                response = await client.get(
+                    ecomail_page_url, params={"status": 1, "per_page": 100, "page": page}
+                )
+                logger.debug(f"Parsing response: {response.url}")
+                response.raise_for_status()
+                response_json = response.json()
+                for subscriber in response_json["data"]:
+                    source = (
+                        "mailchimp" if subscriber["source"] == "mailchimp" else "ecomail"
+                    )
+                    subscribers.setdefault(subscriber["email"], set())
+                    subscribers[subscriber["email"]].add(source)
+                    ecomail_count += 1
+                if response_json["next_page_url"]:
+                    logger.debug(
+                        f"Fetched {ecomail_count} subscribers so far, "
+                        f"{response_json['per_page']} per page, "
+                        f"{response_json['total']} total"
+                    )
+                else:
+                    logger.info(f"Fetched {ecomail_count} subscribers from Ecomail")
+                    break
+        logger.debug("Caching subscribers")
+        cache.set(cache_key, subscribers, expire=3600 * cache_hrs)
+
+    logger.info(f"Adding {len(subscribers)} subscribers to Buttondown")
+    # TODO
+    # for email, sources in subscribers.items():
+    #     try:
+    #         # TODO
+    #         # async with ButtondownAPI() as buttondown:
+    #         #     await buttondown.add_subscriber(email=email, tags=sources)
+    #         logger.debug(f"Added subscriber {email} from {sources}")
+    #     except httpx.HTTPStatusError as e:
+    #         error_data = e.response.json()
+    #         error_code = error_data.get("code")
+    #         if error_code == SubscriberErrorCode.EMAIL_ALREADY_EXISTS:
+    #             logger.debug(f"Subscriber {email} already exists, skipping")
+    #             continue
+    #         logger.error(f"Failed to add subscriber {email} from {sources}: {e}")
+    #     break
