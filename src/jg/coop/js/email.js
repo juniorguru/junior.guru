058a22d23cc77ed71a577c45059cc5f949b1fc1e
Honza Javorek <mail@honzajavorek.cz>
2025-10-22 10:49:26 +0200

    accommodate buttondown specifics

--- src/jg/coop/js/email.js
+++ src/jg/coop/js/email.js
@@ -5,23 +5,21 @@ function setupEmailForm() {
 
   if (!form || !subscribed || !confirmed) return;
 
-  const status = new URLSearchParams(window.location.search).get("status");
-  switch (status) {
-    case "subscribed":
-      form.setAttribute("hidden", "");
-      subscribed.removeAttribute("hidden");
-      confirmed.setAttribute("hidden", "");
-      break;
-    case "confirmed":
-      form.setAttribute("hidden", "");
-      subscribed.setAttribute("hidden", "");
-      confirmed.removeAttribute("hidden");
-      break;
-    default:
-      form.removeAttribute("hidden");
-      subscribed.setAttribute("hidden", "");
-      confirmed.setAttribute("hidden", "");
-      break;
+  const emailAddress = new URLSearchParams(window.location.search).get("email_address");
+  if (emailAddress.includes("@")) {
+    // subscribed: https://junior.guru/news/?email_address=example@example.com
+    form.setAttribute("hidden", "");
+    subscribed.removeAttribute("hidden");
+    confirmed.setAttribute("hidden", "");
+  } else if (emailAddress) {
+    // confirmed: https://junior.guru/news/?email_address=confirmed
+    form.setAttribute("hidden", "");
+    subscribed.setAttribute("hidden", "");
+    confirmed.removeAttribute("hidden");
+  } else {
+    form.removeAttribute("hidden");
+    subscribed.setAttribute("hidden", "");
+    confirmed.setAttribute("hidden", "");
   }
 }
 
@@ -31,7 +29,7 @@ function setupEmailResetButton() {
   resetButton.addEventListener("click", function (event) {
     event.preventDefault();
     const url = new URL(window.location.href);
-    url.searchParams.delete("status");
+    url.searchParams.delete("email_address");
     window.history.pushState({}, "", url.pathname + url.search);
     setupEmailForm();
   });
