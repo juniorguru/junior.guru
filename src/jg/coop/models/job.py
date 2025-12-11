bbff2d0b9de27c2e75677cd6714455711487a484
Honza Javorek <mail@honzajavorek.cz>
2025-12-11 12:38:24 +0100

    dear Honza, please, next time don't change what's not broken

--- src/jg/coop/models/job.py
+++ src/jg/coop/models/job.py
@@ -304,14 +304,12 @@ class ListedJob(BaseModel):
     def company_logo_source_urls(self) -> list[str]:
         return self.company_logo_urls + ([self.company_url] if self.company_url else [])
 
-    def get_logo_source_type(self, logo_source_url: str) -> LogoSourceType:
+    def get_logo_source_type(self, logo_source_url: str) -> LogoSourceType | None:
         if logo_source_url in self.company_logo_urls:
             return LogoSourceType.LOGO
         if logo_source_url == self.company_url:
             return LogoSourceType.ICON
-        raise ValueError(
-            f"Logo source URL {logo_source_url} not found in job's logo sources"
-        )
+        return None
 
     @property
     def tags(self) -> list[Tag]:
