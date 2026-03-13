12facc58671ccee1a44cb16f01c95d2aa81ebd25
Honza Javorek <mail@honzajavorek.cz>
2026-03-13 13:49:27 +0100

    introduce related pages

--- src/jg/coop/models/page.py
+++ src/jg/coop/models/page.py
@@ -111,3 +111,18 @@ class Page(BaseModel):
             .where(cls.src_uri.startswith("stories/"))
             .order_by(cls.date.desc())
         )
+
+    def list_related(self) -> list[Self]:
+        related_by_src_uri = {}
+        for stage_slug in self.stages or []:
+            for related_page in self.stage_listing(stage_slug):
+                related_by_src_uri[related_page.src_uri] = related_page
+
+        related_by_src_uri.pop(self.src_uri, None)
+        return sorted(
+            related_by_src_uri.values(),
+            key=lambda related_page: (
+                related_page.nav_sort_key is None,
+                related_page.nav_sort_key,
+            ),
+        )
