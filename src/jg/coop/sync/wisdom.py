b03556bb8a73e1d573f94979e88f771a791c741e
Honza Javorek <mail@honzajavorek.cz>
2026-04-29 17:31:44 +0200

    convert wisdom to new style of yaml processing

--- src/jg/coop/sync/wisdom.py
+++ src/jg/coop/sync/wisdom.py
@@ -1,38 +1,39 @@
+from datetime import date
 from pathlib import Path
 from textwrap import shorten
 
-from strictyaml import Map, Seq, Str, load
+import click
+import yaml
 
 from jg.coop.cli.sync import main as cli
 from jg.coop.lib import loggers
-from jg.coop.lib.yaml import Date
+from jg.coop.lib.yaml import YAMLConfig
 from jg.coop.models.base import db
 from jg.coop.models.wisdom import Wisdom
-
-
-YAML_PATH = Path("src/jg/coop/data/wisdom.yml")
-
-YAML_SCHEMA = Seq(
-    Map(
-        {
-            "date": Date(),
-            "text": Str(),
-            "name": Str(),
-        }
-    )
-)
 
 
 logger = loggers.from_path(__file__)
 
 
+class WisdomConfig(YAMLConfig):
+    date: date
+    text: str
+    name: str
+
+
 @cli.sync_command()
+@click.option(
+    "--path",
+    default=Path("src/jg/coop/data/wisdom.yml"),
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
+)
 @db.connection_context()
-def main():
+def main(path: Path):
     Wisdom.drop_table()
     Wisdom.create_table()
-    for yaml_record in load(YAML_PATH.read_text(), YAML_SCHEMA):
-        record = yaml_record.data
+    yaml_data = yaml.safe_load(path.read_text())
+    for config in [WisdomConfig(**yaml_record) for yaml_record in yaml_data]:
+        record = config.model_dump()
         logger.info(
             f"{record['name']}: {shorten(record['text'], width=20, placeholder='…')}"
         )
