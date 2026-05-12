095a785792644985bcafde9acdbe4b6c41cf8701
Honza Javorek <mail@honzajavorek.cz>
2026-05-12 09:25:28 +0200

    formatting

--- tests/sync_jobs_scraped_pipelines/test_gender_remover.py
+++ tests/sync_jobs_scraped_pipelines/test_gender_remover.py
@@ -19,40 +19,76 @@ from jg.coop.sync.jobs_scraped.pipelines.gender_remover import process
             "Quantitative Java Developer (m/f/d) for Financial Markets",
             "Quantitative Java Developer for Financial Markets",
         ),
-        ("Search Engine Backend Engineer (m/f)", "Search Engine Backend Engineer"),
+        (
+            "Search Engine Backend Engineer (m/f)",
+            "Search Engine Backend Engineer",
+        ),
         (
             "Computer scientist - Software Architectures for Traffic Systems (f/m/d)",
             "Computer scientist - Software Architectures for Traffic Systems",
         ),
-        ("JAVA BPM SOFTWARE DEVELOPER (M/F)", "JAVA BPM SOFTWARE DEVELOPER"),
-        ("Frontend Engineer (m/f/div)", "Frontend Engineer"),
-        ("Junior Fullstack Developer (m/w/d)", "Junior Fullstack Developer"),
-        ("Java Developer (m/w)", "Java Developer"),
-        ("Software Engineer Ruby/Go (f/m/*)", "Software Engineer Ruby/Go"),
+        (
+            "JAVA BPM SOFTWARE DEVELOPER (M/F)",
+            "JAVA BPM SOFTWARE DEVELOPER",
+        ),
+        (
+            "Frontend Engineer (m/f/div)",
+            "Frontend Engineer",
+        ),
+        (
+            "Junior Fullstack Developer (m/w/d)",
+            "Junior Fullstack Developer",
+        ),
+        (
+            "Java Developer (m/w)",
+            "Java Developer",
+        ),
+        (
+            "Software Engineer Ruby/Go (f/m/*)",
+            "Software Engineer Ruby/Go",
+        ),
         (
             "C/Ada Software Developer & Tester Aviation (m/f/d )(Ref.-Nr.: 2020)",
             "C/Ada Software Developer & Tester Aviation (Ref.-Nr.: 2020)",
         ),
-        ("Solution Engineer (M/F/X)", "Solution Engineer"),
+        (
+            "Solution Engineer (M/F/X)",
+            "Solution Engineer",
+        ),
         (
             "UI Frontend Engineer | React JS Developer f/m/x",
             "UI Frontend Engineer | React JS Developer",
         ),
-        ("Junior iOS Engineer (Swift) (m|f|x)", "Junior iOS Engineer (Swift)"),
-        ("Archer Developer (m/f/diverse)", "Archer Developer"),
+        (
+            "Junior iOS Engineer (Swift) (m|f|x)",
+            "Junior iOS Engineer (Swift)",
+        ),
+        (
+            "Archer Developer (m/f/diverse)",
+            "Archer Developer",
+        ),
         (
             "Tech Trainee Programme (Java-JavaScript-iOS-Android) (d/f/m)",
             "Tech Trainee Programme (Java-JavaScript-iOS-Android)",
         ),
         # French
-        ("Internship: JAVA DEVELOPER - H/F", "Internship: JAVA DEVELOPER"),
+        (
+            "Internship: JAVA DEVELOPER - H/F",
+            "Internship: JAVA DEVELOPER",
+        ),
         # Czech
         (
             "GOlang / PHP backend programátor cloudových služeb (m/ž)",
             "GOlang / PHP backend programátor cloudových služeb",
         ),
-        ("👩‍💻/👨‍💻 Junior Product Designer", "Junior Product Designer"),
-        ("Junior ABAP Developer (all genders)", "Junior ABAP Developer"),
+        (
+            "👩‍💻/👨‍💻 Junior Product Designer",
+            "Junior Product Designer",
+        ),
+        (
+            "Junior ABAP Developer (all genders)",
+            "Junior ABAP Developer",
+        ),
     ],
 )
 @pytest.mark.asyncio
