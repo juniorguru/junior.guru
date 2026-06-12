7f5a74139881ad27e1c87c6dcfc961cf7b13557c
Honza Javorek <mail@honzajavorek.cz>
2026-06-12 11:26:28 +0200

    rewrite dashboard

--- src/jg/coop/sync/guide_dashboard.py
+++ src/jg/coop/sync/guide_dashboard.py
@@ -20,6 +20,45 @@ from jg.coop.models.tip import Tip
 
 IMAGES_DIR = Path("src/jg/coop/images")
 
+HEADER_MESSAGE = (
+    "# Pravidla a tipy\n\n"
+    "Klub je místo, kde můžeš spolu s ostatními posunout svůj rozvoj v oblasti programování, "
+    "nebo s tím pomoci ostatním.\n\n"
+    "👉 Tykáme si\n"
+    "👉 Dodržuj pravidla chování na [junior.guru/coc](https://junior.guru/coc/)"
+)
+
+LINKS_MESSAGE = "## Odkazy"
+
+LINKS_VIEW = ui.View(
+    ui.Button(
+        emoji="💳",
+        label="Nastavení placení",
+        url="https://juniorguru.memberful.com",
+    ),
+    ui.Button(
+        emoji="🤔",
+        label="Časté dotazy",
+        url="https://junior.guru/faq/",
+    ),
+    ui.Button(
+        emoji="💕",
+        label="Pravidla chování",
+        url="https://junior.guru/coc/",
+    ),
+    ui.Button(
+        emoji="💡",
+        label="Zpětná vazba",
+        url=f"https://discord.com/channels/{CLUB_GUILD_ID}/{ClubChannelID.META}",
+    ),
+)
+
+TIPS_HEADER_MESSAGE = (
+    "## Tipy\n\n"
+    "Jak z klubu dostat maximum? Tady je seznam tipů, které ti pomohou se zorientovat. "
+    f"Najdeš je také v <#{ClubChannelID.TIPS}>"
+)
+
 
 logger = loggers.from_path(__file__)
 
@@ -37,67 +76,67 @@ def main(channel_id: int, cache_key: str, cache_days: int, force: bool):
     cache_miss = cache.get(cache_key) != tips_hash
 
     if force or cache_miss:
-        logger.info(f"Recreating channel! (force={force}, cache_miss={cache_miss})")
-        discord_task.run(recreate_channel, channel_id, tips)
+        logger.info(f"Synchronizing channel (force={force}, cache_miss={cache_miss})")
+        discord_task.run(sync_channel, channel_id, tips)
         cache.set(cache_key, tips_hash, expire=86_400 * cache_days)
     else:
         logger.info("Channel is up-to-date")
 
 
-async def recreate_channel(client: ClubClient, channel_id: int, tips: list[Tip]):
+async def sync_channel(client: ClubClient, channel_id: int, tips: list[Tip]):
     channel = await client.fetch_channel(channel_id)
-    with mutating_discord(channel) as proxy:
-        await proxy.purge(limit=None)
-        await proxy.send(
-            (
-                "# Pravidla a tipy\n\n"
-                "Klub je místo, kde můžeš spolu s ostatními posunout svůj rozvoj v oblasti programování, "
-                "nebo s tím pomoci ostatním.\n\n"
-                "👉 Tykáme si\n"
-                "👉 Dodržuj pravidla chování na [junior.guru/coc](https://junior.guru/coc/)"
-            ),
-            suppress=True,
-        )
-        await proxy.send(
-            "## Odkazy",
-            view=ui.View(
-                ui.Button(
-                    emoji="💳",
-                    label="Nastavení placení",
-                    url="https://juniorguru.memberful.com",
-                ),
-                ui.Button(
-                    emoji="🤔",
-                    label="Časté dotazy",
-                    url="https://junior.guru/faq/",
-                ),
-                ui.Button(
-                    emoji="💕",
-                    label="Pravidla chování",
-                    url="https://junior.guru/coc/",
-                ),
-                ui.Button(
-                    emoji="💡",
-                    label="Zpětná vazba",
-                    url=f"https://discord.com/channels/{CLUB_GUILD_ID}/{ClubChannelID.META}",
-                ),
-            ),
-        )
-        await proxy.send(
-            (
-                "## Tipy\n\n"
-                "Jak z klubu dostat maximum? Tady je seznam tipů, které ti pomohou se zorientovat. "
-                f"Najdeš je také v <#{ClubChannelID.TIPS}>"
-            ),
-            suppress=True,
-        )
-    for tip in tips:
-        logger.info(f"Posting {tip.title!r}")
+    messages = [
+        message async for message in channel.history(limit=None, oldest_first=True)
+    ]
+    if messages:
+        header_message = messages[0]
+        with mutating_discord(header_message) as proxy:
+            await proxy.edit(
+                content=HEADER_MESSAGE,
+                embeds=[],
+                suppress=True,
+            )
+        links_message = messages[1] if len(messages) > 1 else None
+        if links_message:
+            with mutating_discord(links_message) as proxy:
+                await proxy.edit(content=LINKS_MESSAGE, embeds=[], view=LINKS_VIEW)
+        tips_header_message = messages[2] if len(messages) > 2 else None
+        if tips_header_message:
+            with mutating_discord(tips_header_message) as proxy:
+                await proxy.edit(
+                    content=TIPS_HEADER_MESSAGE,
+                    embeds=[],
+                    suppress=True,
+                )
+        tip_messages = messages[3:]
+    else:
+        with mutating_discord(channel) as proxy:
+            await proxy.send(content=HEADER_MESSAGE, suppress=True)
+            await proxy.send(content=LINKS_MESSAGE, view=LINKS_VIEW)
+            await proxy.send(content=TIPS_HEADER_MESSAGE, suppress=True)
+        tip_messages = []
+
+    for index, tip in enumerate(tips):
         embed = Embed(
             title=tip.title_text,
             url=tip.discord_url,
             description=tip.lead,
         )
         embed.set_thumbnail(url=emoji_url(tip.emoji))
-        with mutating_discord(channel) as proxy:
-            await proxy.send(embed=embed)
+
+        try:
+            message = tip_messages[index]
+        except IndexError:
+            logger.info(f"Posting new message for {tip.title!r}")
+            with mutating_discord(channel) as proxy:
+                await proxy.send(embed=embed)
+        else:
+            logger.info(f"Updating message for {tip.title!r}")
+            with mutating_discord(message) as proxy:
+                await proxy.edit(embed=embed)
+
+    if extra_messages := tip_messages[len(tips) :]:
+        logger.info(f"Deleting {len(extra_messages)} outdated message(s)")
+        for message in extra_messages:
+            with mutating_discord(message) as proxy:
+                await proxy.delete()
